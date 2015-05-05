e4d673453dcfe3d6f418aff92ffadecfec387a0f
Philipp Paris <paris.philipp@gmail.com>
2015-05-05 23:11:01 +0200

    MenuCategory: removed constructors; added toString() and equals(Object)

---
+++ src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/TestMenuCategoryDAO.java
@@ -0,0 +1,10 @@
+package com.at.ac.tuwien.sepm.ss15.edulium.dao;
+
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.MenuCategory;
+
+/**
+ * Created by phili on 5/5/15.
+ */
+public class TestMenuCategoryDAO {
+
+}
