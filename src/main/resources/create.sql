4510c6f81405b7c119aac2fbd21c6b097e87f1de
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-05-06 23:55:13 +0200

    Fix create.sql (wrong data types, missing references, syntax fails, ...)

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,256 +1,241 @@
-CREATE TABLE RestaurantUser (
-	ID IDENTITY PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS RestaurantUser (
+	ID IDENTITY,
 	name VARCHAR(100),
 	userRole VARCHAR(100),
 	deleted BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE UserHistory (
+CREATE TABLE IF NOT EXISTS RestaurantUserHistory (
+	user_ID BIGINT REFERENCES RestaurantUser(ID),
 	name VARCHAR(100),
 	userRole VARCHAR(100),
-	deleted BOOLEAN DEFAULT FALSE,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
-	changeNr IDENTITY,
-	user_ID FOREIGN KEY REFERENCES RestaurantUser(ID),
-	PRIMARY KEY(changeNr, user_ID)
+	deleted BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
+	PRIMARY KEY(user_ID, changeNr)
 );
 
-CREATE TABLE Invoice (
-	ID IDENTITY PRIMARY KEY,
-	invoiceTime INT,
-	brutto DOUBLE,
-	paid DOUBLE DEFAULT FALSE,
-	canceled BOOLEAN DEFAULT FALSE,
-	cashedWaiter FOREIGN KEY REFERENCES RestaurantUser(ID)
-);
-
-CREATE TABLE InvoiceHistory (
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
-	changeNr IDENTITY,
-	paid DOUBLE,
-	canceled BOOLEAN DEFAULT FALSE,
-	invoice_ID FOREIGN KEY REFERENCES Invoice(ID),
-	PRIMARY KEY(changeNr, invoice_ID)
-);
-
-CREATE TABLE Instalment (
+CREATE TABLE IF NOT EXISTS Invoice (
 	ID IDENTITY,
-	instalmentTime TIME,
+	invoiceTime TIMESTAMP,
+	brutto DECIMAL(20, 2),
+	paid DECIMAL(20, 2),
+	user_ID BIGINT REFERENCES RestaurantUser(ID),
+	canceled BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS InvoiceHistory (
+    invoice_ID BIGINT REFERENCES Invoice(ID),
+    paid DECIMAL(20, 2),
+    canceled BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
+	PRIMARY KEY(invoice_ID, changeNr)
+);
+
+CREATE TABLE IF NOT EXISTS Instalment (
+	ID BIGINT AUTO_INCREMENT,
+	instalmentTime TIMESTAMP,
 	paymentInfo TEXT,
-	type TEXT,
-	amount DOUBLE,
-	invoice_ID FOREIGN KEY REFERENCES Invoice(ID),
+	type VARCHAR(100),
+	amount DECIMAL(20, 2),
+	invoice_ID BIGINT REFERENCES Invoice(ID),
 	PRIMARY KEY(ID, invoice_ID)
 );
 
-CREATE TABLE RestaurantSection (
-	ID IDENTITY PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS RestaurantSection (
+	ID IDENTITY,
 	name VARCHAR(100),
 	deleted BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE SectionHistory (
-	section_ID FOREIGN KEY REFERENCES RestaurantSection(ID),
+CREATE TABLE IF NOT EXISTS RestaurantSectionHistory (
+	section_ID BIGINT REFERENCES RestaurantSection(ID),
 	name VARCHAR(100),
-	deleted BOOLEAN DEFAULT FALSE,
-	changeNr IDENTITY,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
+	deleted BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(section_ID, changeNr)
 );
 
-CREATE TABLE RestaurantTable (
-	waiter_ID FOREIGN KEY REFERENCES RestaurantUser(ID),
-	section_ID FOREIGN KEY REFERENCES RestaurantSection(ID),
+CREATE TABLE IF NOT EXISTS RestaurantTable (
+	section_ID BIGINT REFERENCES RestaurantSection(ID),
+	number BIGINT,
 	seats INT,
-	number IDENTITY,
 	tableRow INT,
-	tableCol INT,
+	tableColumn INT,
+	user_ID BIGINT REFERENCES RestaurantUser(ID),
 	disabled BOOLEAN DEFAULT FALSE,
 	PRIMARY KEY(section_ID, number)
 );
 
-CREATE TABLE TableHistory (
-	section_ID FOREIGN KEY REFERENCES RestaurantSection(ID),
-	table_Number FOREIGN KEY REFERENCES RestaurantTable(number),
+CREATE TABLE IF NOT EXISTS TableHistory (
+	section_ID BIGINT REFERENCES RestaurantSection(ID),
+	table_number BIGINT REFERENCES RestaurantTable(number),
 	seats INT,
-	number INT,
 	tableRow INT,
-	tableCol INT,
-	disabled BOOLEAN DEFAULT FALSE,
-	changeNr IDENTITY,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
-	waiter_ID FOREIGN KEY REFERENCES RestaurantUser(ID),
-	PRIMARY KEY(changeNr, section_ID, table_Number)
+	tableColumn INT,
+	user_ID BIGINT REFERENCES RestaurantUser(ID),
+	disabled BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
+	PRIMARY KEY(section_ID, table_number, changeNr)
 );
 
-CREATE TABLE Reservation (
-	ID IDENTITY PRIMARY KEY,
-	reservationTime TIME,
+CREATE TABLE IF NOT EXISTS Reservation (
+	ID IDENTITY,
+	reservationTime TIMESTAMP,
 	name VARCHAR(100),
 	quantity INT,
-	closed BOOLEAN DEFAULT FALSE,
-	duration INT
-);
-
-CREATE TABLE ReservationHistory (
-	reservation_ID FOREIGN KEY REFERENCES Reservation(ID),
-	reservationTime TIME,
-	name VARCHAR(100),
-	quantity INT,
-	closed BOOLEAN DEFAULT FALSE,
 	duration INT,
-	changeNr IDENTITY,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
+	closed BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS ReservationHistory (
+	reservation_ID BIGINT REFERENCES Reservation(ID),
+	reservationTime TIMESTAMP,
+	name VARCHAR(100),
+	quantity INT,
+	duration INT,
+	closed BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(reservation_ID, changeNr)
 );
 
-CREATE TABLE ReservationAssoc (
-	table_ID FOREIGN KEY REFERENCES RestaurantTable(ID),
-	reservation_ID FOREIGN KEY REFERENCES Reservation(ID),
+CREATE TABLE IF NOT EXISTS ReservationAssoc (
+	table_number BIGINT REFERENCES RestaurantTable(number),
+	reservation_ID BIGINT REFERENCES Reservation(ID),
 	disabled BOOLEAN DEFAULT FALSE,
-	PRIMARY KEY(table_ID, reservation_ID)
+	PRIMARY KEY(table_number, reservation_ID)
 );
 
-CREATE TABLE ReservationAssocHistory (
-	reservationAssoc_table_ID FOREIGN KEY REFERENCES ReservationAssoc(table_ID),
-	reservationAssoc_reservationID FOREIGN KEY REFERENCES ReservationAssoc(reservation_ID),
-	disabled BOOLEAN DEFAULT FALSE,
-	changeNr IDENTITY,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
-	PRIMARY KEY(reservationAssoc_table_ID, reservationAssoc_reservationID, changeNr)
+CREATE TABLE IF NOT EXISTS ReservationAssocHistory (
+	table_number BIGINT REFERENCES RestaurantTable(number),
+	reservation_ID BIGINT REFERENCES Reservation(ID),
+	disabled BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
+	PRIMARY KEY(table_number, reservation_ID, changeNr)
 );
 
-CREATE TABLE MenuCategory (
-	ID IDENTITY PRIMARY KEY,
-	deleted BOOLEAN DEFAULT FALSE,
-	name VARCHAR(100)
-);
-
-CREATE TABLE MenuCategoryHistory (
-	menuCategory_ID FOREIGN KEY REFERENCES MenuCategory(ID),
-	deleted BOOLEAN DEFAULT FALSE,
+CREATE TABLE IF NOT EXISTS MenuCategory (
+	ID IDENTITY,
 	name VARCHAR(100),
-	changeNr IDENTITY,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
-	PRIMARY KEY(menuCategory_ID, changeNr)
-);
-
-CREATE TABLE MenuEntry (
-	ID IDENTITY PRIMARY KEY,
-	name VARCHAR(100),
-	price DOUBLE,
-	available BOOLEAN DEFAULT FALSE,
-	description VARCHAR(300),
-	deleted BOOLEAN
-);
-
-CREATE TABLE MenuEntryHistory (
-	menuEntry_ID FOREIGN KEY REFERENCES MenuEntry(ID),
-	name VARCHAR(100),
-	price DOUBLE,
-	available BOOLEAN DEFAULT FALSE,
-	description VARCHAR(300),
-	deleted BOOLEAN,
-	changeNr IDENTITY,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
-	taxRate DOUBLE,
-	menuCategory FOREIGN KEY REFERENCES MenuCategory(ID),
-	PRIMARY KEY(menuEntry_ID, changeNr)
-);
-
-CREATE TABLE TaxRate (
-	ID IDENTITY PRIMARY KEY,
-	taxRateValue DOUBLE,
 	deleted BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE TaxRateHistory (
-	taxRate_ID FOREIGN KEY REFERENCES TaxRate(ID),
-	taxRateValue DOUBLE,
-	deleted BOOLEAN DEFAULT FALSE,
-	changeNr IDENTITY,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
+CREATE TABLE IF NOT EXISTS MenuCategoryHistory (
+	category_ID BIGINT REFERENCES MenuCategory(ID),
+	name VARCHAR(100),
+	deleted BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
+	PRIMARY KEY(category_ID, changeNr)
+);
+
+CREATE TABLE IF NOT EXISTS TaxRate (
+	ID IDENTITY,
+	taxRateValue DECIMAL(3, 2),
+	deleted BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS TaxRateHistory (
+	taxRate_ID BIGINT REFERENCES TaxRate(ID),
+	taxRateValue DECIMAL(3, 2),
+	deleted BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(taxRate_ID, changeNr)
 );
 
-CREATE TABLE Sale (
-	ID IDENTITY PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS MenuEntry (
+	ID IDENTITY,
+	name VARCHAR(100),
+	price DECIMAL(20, 2),
+	available BOOLEAN,
+	description TEXT,
+	taxRate_ID BIGINT REFERENCES TaxRate(ID),
+    category_ID BIGINT REFERENCES MenuCategory(ID),
+	deleted BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS MenuEntryHistory (
+	menuEntry_ID BIGINT REFERENCES MenuEntry(ID),
+	name VARCHAR(100),
+	price DECIMAL(20, 2),
+	available BOOLEAN,
+	description TEXT,
+	taxRate_ID BIGINT REFERENCES TaxRate(ID),
+    category_ID BIGINT REFERENCES MenuCategory(ID),
+	deleted BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
+	PRIMARY KEY(menuEntry_ID, changeNr)
+);
+
+CREATE TABLE IF NOT EXISTS Sale (
+	ID IDENTITY,
 	name VARCHAR(100),
 	deleted BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE SaleHistory ()
-	sale_ID FOREIGN KEY REFERENCES Sale(ID),
+CREATE TABLE IF NOT EXISTS SaleHistory (
+	sale_ID BIGINT REFERENCES Sale(ID),
 	name VARCHAR(100),
-	deleted BOOLEAN DEFAULT FALSE,
-	changeNr IDENTITY,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
+	deleted BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(sale_ID, changeNr)
-;
+);
 
-CREATE TABLE SaleAssoc (
-	menuEntry_ID FOREIGN KEY REFERENCES MenuEntry(ID),
-	sale_ID FOREIGN KEY REFERENCES Sale(ID),
+CREATE TABLE IF NOT EXISTS SaleAssoc (
+	menuEntry_ID BIGINT REFERENCES MenuEntry(ID),
+	sale_ID BIGINT REFERENCES Sale(ID),
+	salePrice DECIMAL(20, 2),
 	disabled BOOLEAN DEFAULT FALSE,
-	salePrice DOUBLE,
 	PRIMARY KEY(menuEntry_ID, sale_ID)
 );
 
-CREATE TABLE SaleAssocHistory (
-	saleAssoc_menuEntry_ID FOREIGN KEY REFERENCES SaleAssoc(menuEntry_ID),
-	saleAssoc_sale_ID FOREIGN KEY REFERENCES SaleAssoc(sale_ID),
-	disabled BOOLEAN DEFAULT FALSE,
-	salePrice DOUBLE,
-	changeNr IDENTITY,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
-	PRIMARY KEY(saleAssoc_menuEntry_ID, saleAssoc_sale_ID, changeNr)
+CREATE TABLE IF NOT EXISTS SaleAssocHistory (
+	menuEntry_ID BIGINT REFERENCES MenuEntry(ID),
+	sale_ID BIGINT REFERENCES Sale(ID),
+	salePrice DECIMAL(20, 2),
+	disabled BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
+	PRIMARY KEY(menuEntry_ID, sale_ID, changeNr)
 );
 
-CREATE TABLE OnetimeSale (
-	sale_ID FOREIGN KEY REFERENCES Sale(ID),
-	fromTime TIME,
-	toTime TIME,
-	PRIMARY KEY(sale_ID)
+CREATE TABLE IF NOT EXISTS OnetimeSale (
+	sale_ID BIGINT PRIMARY KEY REFERENCES Sale(ID),
+	fromTime TIMESTAMP,
+	toTime TIMESTAMP
 );
 
-CREATE TABLE OnetimeSaleHistory (
-	sale_ID FOREIGN KEY REFERENCES OnetimeSale(sale_ID),
-	fromTime TIME,
-	toTime TIME,
-	changeNr IDENTITY,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
+CREATE TABLE IF NOT EXISTS OnetimeSaleHistory (
+	sale_ID BIGINT REFERENCES Sale(ID),
+	fromTime TIMESTAMP,
+	toTime TIMESTAMP,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(sale_ID, changeNr)
 );
 
-CREATE TABLE IntermittentSale (
-	sale_ID FOREIGN KEY REFERENCES Sale(ID),
-	enabled BOOLEAN,
-	monday BOOLEAN,
-	tuesday BOOLEAN,
-	wednesday BOOLEAN,
-	thursday BOOLEAN,
-	friday BOOLEAN,
-	saturday BOOLEAN,
-	sunday BOOLEAN,
-	fromDayTime TIME,
-	duration INT
-);
-
-CREATE TABLE IntermittentSaleHistory (
-	sale_ID FOREIGN KEY REFERENCES IntermittentSale(ID),
-	enabled BOOLEAN,
+CREATE TABLE IF NOT EXISTS IntermittentSale (
+	sale_ID BIGINT PRIMARY KEY REFERENCES Sale(ID),
 	monday BOOLEAN,
 	tuesday BOOLEAN,
 	wednesday BOOLEAN,
@@ -260,67 +245,82 @@ CREATE TABLE IntermittentSaleHistory (
 	sunday BOOLEAN,
 	fromDayTime TIME,
 	duration INT,
-	changeNr IDENTITY,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
+	enabled BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE IF NOT EXISTS IntermittentSaleHistory (
+	sale_ID BIGINT REFERENCES Sale(ID),
+	monday BOOLEAN,
+	tuesday BOOLEAN,
+	wednesday BOOLEAN,
+	thursday BOOLEAN,
+	friday BOOLEAN,
+	saturday BOOLEAN,
+	sunday BOOLEAN,
+	fromDayTime TIME,
+	duration INT,
+	enabled BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(sale_ID, changeNr)
 );
 
-CREATE TABLE Menu (
-	ID IDENTITY PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Menu (
+	ID IDENTITY,
 	name VARCHAR(100),
 	deleted BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE MenuHistory (
-	menu_ID FOREIGN KEY REFERENCES Menu(ID),
+CREATE TABLE IF NOT EXISTS MenuHistory (
+	menu_ID BIGINT REFERENCES Menu(ID),
 	name VARCHAR(100),
-	deleted BOOLEAN DEFAULT FALSE,
-	changeNr IDENTITY,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
+	deleted BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(menu_ID, changeNr)
 );
 
-CREATE TABLE MenuAssoc (
-	menu_ID FOREIGN KEY REFERENCES Menu(ID),
-	menuEntry_ID FOREIGN KEY REFERENCES MenuEntry(ID),
-	menuPrice DOUBLE,
+CREATE TABLE IF NOT EXISTS MenuAssoc (
+	menu_ID BIGINT REFERENCES Menu(ID),
+	menuEntry_ID BIGINT REFERENCES MenuEntry(ID),
+	menuPrice DECIMAL(20, 2),
 	disabled BOOLEAN DEFAULT FALSE,
 	PRIMARY KEY(menu_ID, menuEntry_ID)
 );
 
-CREATE TABLE MenuAssocHistory (
-	menu_ID FOREIGN KEY REFERENCES MenuAssoc(menu_ID),
-	menuEntry_ID FOREIGN KEY REFERENCES MenuAssoc(menuEntry_ID),
-	menuPrice DOUBLE,
-	disabled BOOLEAN DEFAULT FALSE,
-	changeNr INT,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
+CREATE TABLE IF NOT EXISTS MenuAssocHistory (
+	menu_ID BIGINT REFERENCES Menu(ID),
+	menuEntry_ID BIGINT REFERENCES MenuEntry(ID),
+	menuPrice DECIMAL(20, 2),
+	disabled BOOLEAN,
+	changeTime TIMESTAMP,
+	changeUser BIGINT REFERENCES RestaurantUser(ID),
+	changeNr BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(menu_ID, menuEntry_ID, changeNr)
 );
 
-CREATE TABLE RestaurantOrder (
-	ID INT PRIMARY KEY,
-	invoice_ID FOREIGN KEY REFERENCES Invoice(ID),
-	table_ID FOREIGN KEY REFERENCES RestaurantTable(ID),
-	menuEntry_ID FOREIGN KEY REFERENCES MenuEntry(ID),
-	orderTime TIME,
-	canceled BOOLEAN DEFAULT FALSE,
-	brutto DOUBLE,
-	tax DOUBLE,
-	info VARCHAR(100)
+CREATE TABLE IF NOT EXISTS RestaurantOrder (
+	ID IDENTITY,
+	invoice_ID BIGINT REFERENCES Invoice(ID),
+	table_number BIGINT REFERENCES RestaurantTable(number),
+	menuEntry_ID BIGINT REFERENCES MenuEntry(ID),
+	orderTime TIMESTAMP,
+	brutto DECIMAL(20, 2),
+	tax DECIMAL(3, 2),
+	info TEXT,
+	canceled BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE OrderHistory (
-	order_ID FOREIGN KEY REFERENCES RestaurantOrder,
-	changeNr INT,
-	changeTime TIME,
-	changeUser FOREIGN KEY REFERENCES RestaurantUser(ID),
-	canceled BOOLEAN DEFAULT FALSE,
-	info VARCHAR(100),
-	invoice_ID FOREIGN KEY REFERENCES Invoice(ID),
-	table_ID FOREIGN KEY REFERENCES RestaurantTable(ID),
-	PRIMARY KEY(order_ID, changeNr)
+CREATE TABLE IF NOT EXISTS RestaurantOrderHistory (
+	restaurantOrder_ID BIGINT REFERENCES RestaurantOrder(ID),
+	invoice_ID BIGINT REFERENCES Invoice(ID),
+	table_number BIGINT REFERENCES RestaurantTable(number),
+	canceled BOOLEAN,
+	info TEXT,
+    changeTime TIMESTAMP,
+    changeUser BIGINT REFERENCES RestaurantUser(ID),
+    changeNr BIGINT AUTO_INCREMENT,
+	PRIMARY KEY(restaurantOrder_ID, changeNr)
 );
