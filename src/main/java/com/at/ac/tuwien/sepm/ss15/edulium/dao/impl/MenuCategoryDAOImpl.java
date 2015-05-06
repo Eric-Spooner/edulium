c4aec1c443bf5159466f5a910aaa890400ff1593
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-05-06 12:00:49 +0200

    * Add autowiring to MenuCategoryDAOImpl and TestMenuCategoryDao * New dependency "spring-test"

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/impl/MenuCategoryDAOImpl.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/impl/MenuCategoryDAOImpl.java
@@ -3,6 +3,8 @@ package com.at.ac.tuwien.sepm.ss15.edulium.dao.impl;
 import com.at.ac.tuwien.sepm.ss15.edulium.dao.DAOException;
 import com.at.ac.tuwien.sepm.ss15.edulium.dao.MenuCategoryDAO;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.MenuCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -10,12 +12,10 @@ import java.util.List;
 /**
  * H2 Database Implementation of the MenuCategoryDAO interface
  */
+@Repository
 public class MenuCategoryDAOImpl implements MenuCategoryDAO {
+    @Autowired
     private DataSource dataSource;
-
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 
     @Override
     public void create(MenuCategory menuCategory) throws DAOException {
