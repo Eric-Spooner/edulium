e2023b6ff77f8db4c6d92385ce190dac5065cdc5
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-05-10 21:53:09 +0200

    Add abstract base class for domain tests (includes domain context configuration)

---
+++ src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/AbstractDomainTest.java
@@ -0,0 +1,10 @@
+package com.at.ac.tuwien.sepm.ss15.edulium.domain;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:Spring-Domain.xml")
+public abstract class AbstractDomainTest {
+}
