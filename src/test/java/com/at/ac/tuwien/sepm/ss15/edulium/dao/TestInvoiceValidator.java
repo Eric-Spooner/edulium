4012f50a7bcfb909f920dc52e7d2dc579344402f
Philippos Gorgoris <philipgorgoris@gmail.com>
2015-05-12 15:03:10 +0200

    Added more test cases for installments and creator

--- src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/TestInvoiceValidator.java
+++ src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/TestInvoiceValidator.java
@@ -1,6 +1,8 @@
 package com.at.ac.tuwien.sepm.ss15.edulium.dao;
 
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.Installment;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.Invoice;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.User;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.ValidationException;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.Validator;
 import org.junit.Test;
@@ -11,6 +13,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Test cases for the invoice validator
@@ -27,6 +31,33 @@ public class TestInvoiceValidator {
         Invoice invoice = new Invoice();
         invoice.setTime(LocalDateTime.now());
         invoice.setGross(new BigDecimal("23"));
+        invoice.setCreator(new User());
+
+        invoiceValidator.validateForCreate(invoice);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testValidateForCreate_shouldFailWithNullCreator() throws ValidationException {
+        Invoice invoice = new Invoice();
+        invoice.setTime(LocalDateTime.now());
+        invoice.setGross(new BigDecimal("23"));
+
+        invoiceValidator.validateForCreate(invoice);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testValidateForCreate_shouldFailWithProvidedInstallments() throws ValidationException {
+        Invoice invoice = new Invoice();
+        invoice.setTime(LocalDateTime.now());
+        invoice.setGross(new BigDecimal("23"));
+        invoice.setCreator(new User());
+
+        Installment in1 = new Installment();
+        Installment in2 = new Installment();
+        List<Installment> inList = new ArrayList<Installment>();
+        inList.add(in1);
+        inList.add(in2);
+        invoice.setInstallments(inList);
 
         invoiceValidator.validateForCreate(invoice);
     }
