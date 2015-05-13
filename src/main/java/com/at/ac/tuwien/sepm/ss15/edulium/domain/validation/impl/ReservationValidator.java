99eee2496992a61169654f2b41e0e034b84f283b
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-05-13 11:52:38 +0200

    Add a reservation validator

---
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/validation/impl/ReservationValidator.java
@@ -0,0 +1,30 @@
+package com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.impl;
+
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.Reservation;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.ValidationException;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.Validator;
+
+/**
+ * validator implementation for the Reservation domain object
+ */
+public class ReservationValidator implements Validator<Reservation> {
+    @Override
+    public void validateForCreate(Reservation object) throws ValidationException {
+
+    }
+
+    @Override
+    public void validateForUpdate(Reservation object) throws ValidationException {
+
+    }
+
+    @Override
+    public void validateForDelete(Reservation object) throws ValidationException {
+
+    }
+
+    @Override
+    public void validateIdentity(Reservation object) throws ValidationException {
+
+    }
+}
