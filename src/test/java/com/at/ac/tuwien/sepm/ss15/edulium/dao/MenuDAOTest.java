21386dd7c054a4f490ad44b3568733f80f285345
Eric.Moerth <e1325306@student.tuwien.ac.at>
2015-05-13 08:56:39 +0200

    Starting implementing MenuDAO Tests

--- src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/MenuDAOTest.java
+++ src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/MenuDAOTest.java
@@ -159,7 +159,7 @@ public class MenuDAOTest extends AbstractDAOTest {
         LinkedList<MenuEntry> list = new LinkedList<MenuEntry>();
         list.add(createMenuEntry("entry", "desc", "cat", 20, 0.2, true));
         Menu men = createMenu("Menu", list);
-        menuEntryDAO.create(men);
+        menuDAO.create(men);
 
         // check if entry created
         List<Menu> objects = menuDAO.find(Menu.withIdentity(men.getIdentity()));
@@ -171,18 +171,76 @@ public class MenuDAOTest extends AbstractDAOTest {
 
         // THEN
         // check if entry was removed
-        assertTrue(menuEntryDAO.find(MenuEntry.withIdentity(entry.getIdentity())).isEmpty());
-        assertTrue(menuEntryDAO.getAll().isEmpty());
+        assertTrue(menuDAO.find(Menu.withIdentity(men.getIdentity())).isEmpty());
+        assertTrue(menuDAO.getAll().isEmpty());
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testDelete_deletingObjectWithIdentityNullShouldFail() throws DAOException, ValidationException {
+        // GIVEN
+        Menu men = new Menu();
+
+        // WHEN
+        menuDAO.delete(men);
     }
 
     @Test(expected = DAOException.class)
     public void testDelete_deletingNotPersistentObjectShouldFail() throws DAOException, ValidationException {
+        // GIVEN
+        Long identity = (long) 1;
+        Menu men = new Menu();
+        men.setIdentity(identity);
 
+        // generate identity which is not used by any persistent object
+        try {
+            while (!menuDAO.find(men).isEmpty()) {
+                identity++;
+                men.setIdentity(identity);
+            }
+        } catch (DAOException e) {
+            // exception should not occur here
+            fail();
+        }
+
+        // WHEN
+        menuDAO.delete(men);
     }
 
     @Test
     public void testFind_byIdentityShouldReturnObject() throws DAOException, ValidationException {
+        // GIVEN
+        LinkedList<MenuEntry> list1 = new LinkedList<MenuEntry>();
+        list1.add(createMenuEntry("entry1", "desc", "cat", 20, 0.2, true));
+        Menu men1 = createMenu("Menu1", list1);
+        menuDAO.create(men1);
 
+        LinkedList<MenuEntry> list2 = new LinkedList<MenuEntry>();
+        list2.add(createMenuEntry("entry1", "desc", "cat", 20, 0.2, true));
+        Menu men2 = createMenu("Menu2", list2);
+        menuDAO.create(men2);
+
+        LinkedList<MenuEntry> list3 = new LinkedList<MenuEntry>();
+        list3.add(createMenuEntry("entry1", "desc", "cat", 20, 0.2, true));
+        Menu men3 = createMenu("Menu3", list3);
+        menuDAO.create(men3);
+
+        // WHEN
+        List<Menu> objects = menuDAO.find(Menu.withIdentity(men1.getIdentity()));
+        // THEN
+        assertEquals(1, objects.size());
+        assertEquals(men1, objects.get(0));
+
+        // WHEN
+        List<Menu> objects2 = menuDAO.find(Menu.withIdentity(men2.getIdentity()));
+        // THEN
+        assertEquals(1, objects.size());
+        assertEquals(men2, objects.get(0));
+
+        // WHEN
+        List<Menu> objects3 = menuDAO.find(Menu.withIdentity(men3.getIdentity()));
+        // THEN
+        assertEquals(1, objects.size());
+        assertEquals(men3, objects.get(0));
     }
 
     @Test
