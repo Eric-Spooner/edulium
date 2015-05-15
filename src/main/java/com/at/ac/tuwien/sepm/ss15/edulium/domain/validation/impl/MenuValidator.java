d9ef26c4437a40ee3f6a8ad28cbed1270590ea1a
Eric.Moerth <e1325306@student.tuwien.ac.at>
2015-05-15 08:50:15 +0200

    MenuValidator Implementation

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/validation/impl/MenuValidator.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/validation/impl/MenuValidator.java
@@ -10,17 +10,50 @@ import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.Validator;
 public class MenuValidator implements Validator<Menu> {
     @Override
     public void validateForCreate(Menu object) throws ValidationException {
+        if(object == null) {
+            throw new ValidationException("Menu must not be null");
+        }
+        checkForRequiredDataAttributesForCreateAndUpdate(object);
     }
 
     @Override
     public void validateForUpdate(Menu object) throws ValidationException {
+        if (object == null) {
+            throw new ValidationException("Menu must not be null");
+        }
+        validateIdentity(object);
+        checkForRequiredDataAttributesForCreateAndUpdate(object);
     }
 
     @Override
     public void validateForDelete(Menu object) throws ValidationException {
+        if (object == null) {
+            throw new ValidationException("Menu must not be null");
+        }
+
+        validateIdentity(object);
     }
 
     @Override
     public void validateIdentity(Menu object) throws ValidationException {
+        if (object == null) {
+            throw new ValidationException("Menu must not be null");
+        }
+        if (object.getIdentity() == null) {
+            throw new ValidationException("Identity must not be null");
+        }
+    }
+
+    private void checkForRequiredDataAttributesForCreateAndUpdate(Menu object) throws ValidationException {
+        if(object.getName() == null){
+            throw new ValidationException("Menu name must not be null");
+        }else if(object.getName() == ""){
+            throw new ValidationException("Menu name must not be empty");
+        }
+        if(object.getEntries() == null){
+            throw new ValidationException("Menu entries not be null");
+        }else if(object.getEntries().size() == 0){
+            throw new ValidationException("There should be at least one menu entry");
+        }
     }
 }
