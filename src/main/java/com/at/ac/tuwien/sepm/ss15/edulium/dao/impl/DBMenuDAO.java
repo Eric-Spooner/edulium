ef450252c85b271d6a467113a81f12bccf86452c
Eric.Moerth <e1325306@student.tuwien.ac.at>
2015-05-15 09:16:48 +0200

    Menu DAO Implementation Create

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/impl/DBMenuDAO.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/impl/DBMenuDAO.java
@@ -11,6 +11,7 @@ import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.Validator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.sql.DataSource;
 import java.sql.PreparedStatement;
@@ -44,11 +45,30 @@ public class DBMenuDAO implements DAO<Menu> {
                 menu.setIdentity(key.getLong(1));
             }
             key.close();
+            stmt.setLong(1,menu.getIdentity());
             stmt.setString(2, menu.getName());
             stmt.executeUpdate();
         } catch (SQLException e) {
             LOGGER.error("inserting menu into database failed", e);
             throw new DAOException("inserting menu into database failed", e);
+        }
+
+        final String query2 = "INSERT INTO MenuAssoc (name) VALUES (?)";
+/*
+        menu_ID BIGINT REFERENCES Menu(ID),
+                menuEntry_ID BIGINT REFERENCES MenuEntry(ID),
+                menuPrice DECIMAL(20, 2),
+  */
+        for(MenuEntry entry:menu.getEntries()) {
+            try (PreparedStatement stmt = dataSource.getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+                stmt.setLong(1, menu.getIdentity());
+                stmt.setLong(2,entry.getIdentity());
+                stmt.setBigDecimal(3,entry.getPrice());
+                stmt.executeUpdate();
+            } catch (SQLException e) {
+                LOGGER.error("inserting menu association to menuEntry into database failed", e);
+                throw new DAOException("inserting menu association to menuEntry into database failed", e);
+            }
         }
         //generateHistory(menuCategory);
     }
@@ -75,5 +95,32 @@ public class DBMenuDAO implements DAO<Menu> {
     @Override
     public List<History<Menu>> getHistory(Menu object) throws DAOException, ValidationException {
         return null;
+    }
+
+    /**
+     * writes the changes of the dataset into the database
+     * stores the time; number of the change and the user which executed
+     * the changes
+     * @param menu updated dataset
+     * @throws DAOException if an error accessing the database occurred
+     */
+    private void generateHistory(Menu menu) throws DAOException {
+        LOGGER.debug("entering generateHistory with parameters " + menu);
+
+        final String query = "INSERT INTO MenuCategoryHistory " +
+                "(SELECT *, CURRENT_TIMESTAMP(), ?, " +
+                "(SELECT ISNULL(MAX(changeNr) + 1, 1) FROM MenuCategoryHistory Menu ID = ?) " +
+                "FROM Menu WHERE ID = ?)";
+
+        try (PreparedStatement stmt = dataSource.getConnection().prepareStatement(query)) {
+            stmt.setString(1, SecurityContextHolder.getContext().getAuthentication().getName()); // user
+            stmt.setLong(2, menuCategory.getIdentity());          // dataset id
+            stmt.setLong(3, menuCategory.getIdentity());          // dataset id
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.error("generating history failed", e);
+            throw new DAOException("generating history failed", e);
+        }
     }
 }
