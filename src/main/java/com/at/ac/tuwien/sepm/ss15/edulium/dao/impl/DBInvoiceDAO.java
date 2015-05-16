10000fd6df3f22e3d91d5724bbec4b6341e2f7be
Philippos Gorgoris <philipgorgoris@gmail.com>
2015-05-16 18:23:20 +0200

    Implemented create() and update()

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/impl/DBInvoiceDAO.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/impl/DBInvoiceDAO.java
@@ -5,7 +5,16 @@ import com.at.ac.tuwien.sepm.ss15.edulium.dao.DAOException;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.Invoice;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.history.History;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.ValidationException;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.Validator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 /**
@@ -13,13 +22,41 @@ import java.util.List;
  */
 class DBInvoiceDAO implements DAO<Invoice> {
 
+    private static final Logger LOGGER = LogManager.getLogger(DBInvoiceDAO.class);
+
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private Validator<Invoice> invoiceValidator;
+
     /**
      * Writes the invoice object into the database and sets the identity
      * parameter that was generated there
      */
     @Override
     public void create(Invoice invoice) throws DAOException, ValidationException {
-        // TODO: Implement after writing the tests
+        LOGGER.debug("Entering create with parameters: " + invoice);
+
+        invoiceValidator.validateForCreate(invoice);
+
+        final String query = "INSERT INTO Invoice (invoiceTime, brutto, user_ID) " +
+                "VALUES (?, ?, ?);";
+        try (PreparedStatement stmt = dataSource.getConnection()
+                .prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setObject(1, invoice.getTime());
+            stmt.setBigDecimal(2, invoice.getGross());
+            stmt.setString(3, invoice.getCreator().getIdentity());
+            stmt.executeUpdate();
+
+            ResultSet key = stmt.getGeneratedKeys();
+            if (key.next()) {
+                invoice.setIdentity(key.getLong(1));
+            }
+            key.close();
+        } catch (SQLException e) {
+            LOGGER.error("Failed to insert invoice entry into database", e);
+            throw new DAOException("Failed to insert invoice entry into database", e);
+        }
     }
 
     /**
@@ -27,7 +64,25 @@ class DBInvoiceDAO implements DAO<Invoice> {
      */
     @Override
     public void update(Invoice invoice) throws DAOException, ValidationException {
-        // TODO: Implement after writing the tests
+        LOGGER.debug("Entering update with parameters: " + invoice);
+
+        invoiceValidator.validateForUpdate(invoice);
+
+        final String query = "UPDATE Invoice SET invoiceTime = ?, brutto = ?, user_ID = ? " +
+                "WHERE id = ?";
+        try (PreparedStatement stmt = dataSource.getConnection().prepareStatement(query)) {
+            stmt.setObject(1, invoice.getTime());
+            stmt.setBigDecimal(2, invoice.getGross());
+            stmt.setString(3, invoice.getCreator().getIdentity());
+
+            if (stmt.executeUpdate() == 0) {
+                LOGGER.error("Failed to update invoice entry in database, dataset not found");
+                throw new DAOException("Failed to update invoice entry in database, dataset not found");
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Failed to update invoice entry in database", e);
+            throw new DAOException("Failed to update invoice entry in database", e);
+        }
     }
 
     /**
