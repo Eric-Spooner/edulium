5e875a6208e8ee05b81d7de97678c03d3c38b5e5
Philippos Gorgoris <philipgorgoris@gmail.com>
2015-05-16 15:05:49 +0200

    Added missing test cases for create, updade, delete and for getHistory

--- src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/TestInvoiceDAO.java
+++ src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/TestInvoiceDAO.java
@@ -2,12 +2,15 @@ package com.at.ac.tuwien.sepm.ss15.edulium.dao;
 
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.Invoice;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.User;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.history.History;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.ValidationException;
 import org.junit.*;
 import static org.junit.Assert.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -40,6 +43,22 @@ public class TestInvoiceDAO extends AbstractDAOTest {
     }
 
     @Test
+    public void testCreate_addingNullObjectShouldFail() throws ValidationException, DAOException {
+        // WHEN/THEN
+        invoiceDAO.create(null);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testCreate_addingInvalidObjectShouldFail() throws ValidationException, DAOException {
+        // GIVEN
+        Invoice invoice = new Invoice();
+        invoice.setGross(new BigDecimal("5"));
+
+        // WHEN/THEN
+        invoiceDAO.create(invoice);
+    }
+
+    @Test
     public void testUpdate_shouldUpdateObject() throws ValidationException, DAOException {
         // GIVEN
         Invoice invoice = new Invoice();
@@ -59,6 +78,44 @@ public class TestInvoiceDAO extends AbstractDAOTest {
         assertEquals(invoice, invoiceList.get(0));
     }
 
+    @Test(expected = ValidationException.class)
+    public void testUpdate_updatingNullObjectShouldFail() throws ValidationException, DAOException {
+        // WHEN
+        invoiceDAO.update(null);
+    }
+
+    @Test(expected = DAOException.class)
+    public void testUpdate_updatingNotPersistentObjectShouldFail() throws ValidationException, DAOException {
+        // GIVEN
+        Long identity = 1L;
+
+        while (!invoiceDAO.find(Invoice.withIdentity(identity)).isEmpty()) {
+            identity++;
+        }
+
+        // WHEN/THEN
+        Invoice invoice = new Invoice();
+        invoice.setIdentity(identity);
+        invoice.setGross(new BigDecimal("2"));
+        invoice.setTime(LocalDateTime.now());
+        invoice.setCreator(User.withIdentity("TestUser"));
+        invoice.setPaid(Boolean.TRUE);
+        invoiceDAO.update(invoice);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testUpdate_updatingObjectWithoutIdentityShouldFail() throws ValidationException, DAOException {
+        // GIVEN
+        Invoice invoice = new Invoice();
+        invoice.setGross(new BigDecimal("20"));
+        invoice.setTime(LocalDateTime.now());
+        invoice.setPaid(Boolean.TRUE);
+        invoice.setCreator(User.withIdentity("TestUser"));
+
+        // WHEN/THEN
+        invoiceDAO.update(invoice);
+    }
+
     @Test
     public void testDelete_shouldDeleteObject() throws ValidationException, DAOException {
         // GIVEN
@@ -75,6 +132,25 @@ public class TestInvoiceDAO extends AbstractDAOTest {
         // THEN
         Long identity = invoice.getIdentity();
         assertTrue(invoiceDAO.find(Invoice.withIdentity(identity)).isEmpty());
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testDelete_deletingNullObjectShouldFail() throws ValidationException, DAOException {
+        // WHEN
+        invoiceDAO.delete(null);
+    }
+
+    @Test(expected = DAOException.class)
+    public void testDelete_deletingNotPersistentObjectShouldFail() throws ValidationException, DAOException {
+        // GIVEN
+        Long identity = 1L;
+
+        while (!invoiceDAO.find(Invoice.withIdentity(identity)).isEmpty()) {
+            identity++;
+        }
+
+        // WHEN/THEN
+        invoiceDAO.delete(Invoice.withIdentity(identity));
     }
 
     @Test
@@ -186,5 +262,87 @@ public class TestInvoiceDAO extends AbstractDAOTest {
         assertTrue(all.contains(inv1));
         assertTrue(all.contains(inv2));
         assertTrue(all.contains(inv3));
+    }
+
+    @Test
+    public void testGetHistory_shouldReturnObject() throws ValidationException, DAOException {
+        User user = getCurrentUser();
+
+        // GIVEN
+        // Create
+        Invoice invoiceA = new Invoice();
+        LocalDateTime createTime = LocalDateTime.now();
+        invoiceA.setGross(new BigDecimal("20.5"));
+        invoiceA.setTime(createTime);
+        invoiceA.setCreator(user);
+        invoiceDAO.create(invoiceA);
+
+        // Update
+        Invoice invoiceB = Invoice.withIdentity(invoiceA.getIdentity());
+        LocalDateTime updateTime = LocalDateTime.now();
+        invoiceB.setGross(new BigDecimal("24"));
+        invoiceDAO.update(invoiceB);
+
+        // Delete
+        LocalDateTime deleteTime = LocalDateTime.now();
+        invoiceDAO.delete(invoiceB);
+
+        // WHEN
+        List<History<Invoice>> history = invoiceDAO.getHistory(invoiceA);
+
+        // THEN
+        assertEquals(3, history.size());
+        History<Invoice> event;
+
+        // Create history inspection
+        event = history.get(0);
+        assertEquals(Long.valueOf(1), event.getChangeNumber());
+        assertEquals(invoiceA, event.getData());
+        assertEquals(user, event.getUser());
+        assertTrue(Duration.between(createTime, event.getTimeOfChange()).getSeconds() < 1);
+        assertFalse(event.isDeleted());
+
+        // Update history inspection
+        event = history.get(1);
+        assertEquals(Long.valueOf(2), event.getChangeNumber());
+        assertEquals(invoiceB, event.getData());
+        assertEquals(user, event.getUser());
+        assertTrue(Duration.between(updateTime, event.getTimeOfChange()).getSeconds() < 1);
+        assertFalse(event.isDeleted());
+
+        // Delete history inspection
+        event = history.get(3);
+        assertEquals(Long.valueOf(3), event.getChangeNumber());
+        assertEquals(invoiceB, event.getData());
+        assertEquals(user, event.getUser());
+        assertTrue(Duration.between(deleteTime, event.getTimeOfChange()).getSeconds() < 1);
+        assertTrue(event.isDeleted());
+    }
+
+    @Test
+    public void testGetHistory_nonPersistentDataShouldReturnEmptyList() throws ValidationException, DAOException {
+        // GIVEN
+        Long identity = 1L;
+
+        while(!invoiceDAO.find(Invoice.withIdentity(identity)).isEmpty()) {
+            identity++;
+        }
+
+        // WHEN/THEN
+        assertTrue(invoiceDAO.getHistory(Invoice.withIdentity(identity)).isEmpty());
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testGetHistory_shouldFailWithNullObject() throws ValidationException, DAOException {
+        invoiceDAO.getHistory(null);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testGetHistory_shouldFailWithoutIdentity() throws ValidationException, DAOException {
+        // GIVEN
+        Invoice invoice = new Invoice();
+
+        // WHEN
+        invoiceDAO.getHistory(invoice);
     }
 }
