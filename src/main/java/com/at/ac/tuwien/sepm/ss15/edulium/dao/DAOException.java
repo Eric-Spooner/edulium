129de2416c63d68922df030d3cf18d607797229d
Eric.Moerth <e1325306@student.tuwien.ac.at>
2015-05-18 12:03:29 +0200

    MenuDAO find by MenuEntries

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/DAOException.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/DAOException.java
@@ -9,10 +9,6 @@ public class DAOException extends Exception {
         super(message);
     }
 
-    public DAOException(Throwable cause) {
-        super(cause);
-    }
-
     public DAOException(String message, Throwable cause) {
         super(message, cause);
     }
--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/validation/ValidationException.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/validation/ValidationException.java
@@ -4,9 +4,6 @@ package com.at.ac.tuwien.sepm.ss15.edulium.domain.validation;
  * Exception indicating a validation error
  */
 public class ValidationException extends Exception {
-    public ValidationException() {
-    }
-
     public ValidationException(String message) {
         super(message);
     }
