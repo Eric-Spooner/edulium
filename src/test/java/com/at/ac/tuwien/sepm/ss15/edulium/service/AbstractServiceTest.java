b2ced99c5e3d95a5d913b14f838b2816e374c13d
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-05-18 23:53:41 +0200

    Add abstart service test class

---
+++ src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/service/AbstractServiceTest.java
@@ -0,0 +1,17 @@
+package com.at.ac.tuwien.sepm.ss15.edulium.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Base class for all Service Tests which loads the service context configuration.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/Spring-Service.xml")
+@WithMockUser(username="servicetester")
+@Transactional
+public abstract class AbstractServiceTest {
+}
