0133ba49061ea09e896bf6bf9b0de657e3833f7a
Eric.Moerth <e1325306@student.tuwien.ac.at>
2015-05-20 22:33:19 +0200

    Manager Controller Service ineraction

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/gui/ManagerController.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/gui/ManagerController.java
@@ -1,18 +1,30 @@
 package com.at.ac.tuwien.sepm.ss15.edulium.gui;
 
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.MenuCategory;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.MenuEntry;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.TaxRate;
+import com.at.ac.tuwien.sepm.ss15.edulium.service.MenuService;
+import com.at.ac.tuwien.sepm.ss15.edulium.service.Service;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static javafx.collections.FXCollections.observableArrayList;
 
 /**
  * Controller used for the Manager View
@@ -20,10 +32,64 @@ import java.util.ResourceBundle;
 public class ManagerController implements Initializable {
     private static final Logger LOGGER = LogManager.getLogger(ManagerController.class);
 
+    @Autowired
+    private MenuService menuService;
+
+    @FXML
+    private TableView<TaxRate> tableViewTaxRate;
+    @FXML
+    private TableColumn<TaxRate,Long> tableColTaxRateID;
+    @FXML
+    private TableColumn<TaxRate,BigDecimal> tableColTaxRateValue;
+
+    @FXML
+    private TableView<MenuCategory> tableViewMenuCategory;
+    @FXML
+    private TableColumn<MenuCategory,Long> tableColMenuCategoryID;
+    @FXML
+    private TableColumn<MenuCategory,String> tableColMenuCategoryName;
+
+    @FXML
+    private TableView<MenuEntry> tableViewMenuEntry;
+    @FXML
+    private TableColumn<MenuEntry,Long> tableColMenuEntryId;
+    @FXML
+    private TableColumn<MenuEntry,String> tableColMenuEntryName;
+    @FXML
+    private TableColumn<MenuEntry,BigDecimal> tableColMenuEntryPrice;
+    @FXML
+    private TableColumn<MenuEntry,Long> tableColMenuEntryCategory;
+    @FXML
+    private TableColumn<MenuEntry,Long> tableColMenuEntryTaxRate;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources){
+        try {
+            //tableViewTaxRate.setItems(observableArrayList(menuService.get));
+            tableColTaxRateID.setCellValueFactory(new PropertyValueFactory<TaxRate, Long>("identity"));
+            tableColTaxRateValue.setCellValueFactory(new PropertyValueFactory<TaxRate, BigDecimal>("value"));
 
+            if(menuService == null){
+                throw new Exception("menu Service = null");
+            }
+            if(menuService.getAllMenuCategories() != null) {
+                tableViewMenuCategory.setItems(observableArrayList(menuService.getAllMenuCategories()));
+            }
+            tableColMenuCategoryID.setCellValueFactory(new PropertyValueFactory<MenuCategory, Long>("identity"));
+            tableColMenuCategoryName.setCellValueFactory(new PropertyValueFactory<MenuCategory, String>("name"));
+
+            if(menuService.getAllMenuEntries() != null) {
+                tableViewMenuEntry.setItems(observableArrayList(menuService.getAllMenuEntries()));
+            }
+            tableColMenuEntryId.setCellValueFactory(new PropertyValueFactory<MenuEntry, Long>("identity"));
+            tableColMenuEntryName.setCellValueFactory(new PropertyValueFactory<MenuEntry, String>("name"));
+            tableColMenuEntryPrice.setCellValueFactory(new PropertyValueFactory<MenuEntry, BigDecimal>("price"));
+            tableColMenuEntryCategory.setCellValueFactory(new PropertyValueFactory<MenuEntry, Long>("category.id"));
+            tableColMenuEntryTaxRate.setCellValueFactory(new PropertyValueFactory<MenuEntry, Long>("taxRate.id"));
+        }catch (Exception e){
+            LOGGER.error("Initialize Manager View Fail" + e);
+        }
     }
 
     public void buttonEmployeesAddClicked(ActionEvent actionEvent) {
--- src/test/resources/database/testdata.sql
+++ src/test/resources/database/testdata.sql
@@ -1,2 +1,6 @@
 INSERT INTO RestaurantUser (ID, name, userRole) VALUES ('daotester', 'DAO Testuser', 'admin');
 INSERT INTO RestaurantUser (ID, name, userRole) VALUES ('servicetester', 'Service Testuser', 'manager');
+
+INSERT INTO MenuCategory (name) VALUES ('starter');
+INSERT INTO MenuCategory (name) VALUES ('main dish');
+INSERT INTO MenuCategory (name) VALUES ('dessert');
