42974eee3943f9d1f614b60e2371f9d3e2f0f011
Max Landauer <max.landauer@live.at>
2015-05-20 16:35:38 +0200

    Readd Main from master

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/Main.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/Main.java
@@ -1,23 +1,14 @@
 package com.at.ac.tuwien.sepm.ss15.edulium;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         primaryStage.setWidth(300);
         primaryStage.setHeight(300);
         primaryStage.setTitle("Edulium");
         primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
