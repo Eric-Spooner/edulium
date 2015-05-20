a54bfb66f049aae523a4b2c2dd405df569fcbb44
Eric.Moerth <e1325306@student.tuwien.ac.at>
2015-05-20 19:30:11 +0200

    Manager Controller Added

---
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/gui/DialogTaxRateController.java
@@ -0,0 +1,23 @@
+package com.at.ac.tuwien.sepm.ss15.edulium.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Controller for the TaxRate Dialog
+ */
+public class DialogTaxRateController implements Initializable{
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+
+    }
+
+    public void buttonOKClick(ActionEvent actionEvent) {
+    }
+
+    public void buttonCancleClick(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/gui/ManagerController.java
@@ -0,0 +1,78 @@
+package com.at.ac.tuwien.sepm.ss15.edulium.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Controller used for the Manager View
+ */
+public class ManagerController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+
+    }
+
+    public void buttonEmployeesAddClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonEmployeesUpdateClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonEmployeesSearchClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonEmployeesRemoveClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMenuUpdateClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMenuSearchClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMenuRemoveClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMenuAddClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMenuEntryRemoveClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMenuEntrySearchClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMenuEntryUpdateClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMenuEntryAddClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMenuCategoryRemoveClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMenuCategorySearchClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMenuCategoryUpdateClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMenuCategoryAddClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonTaxRateRemoveClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonTaxRateSearchClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonTaxRateUpdateClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonTaxRateAddClicked(ActionEvent actionEvent) {
+    }
+}
