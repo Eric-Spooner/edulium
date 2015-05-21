cb49f2e561a0dda3cc7391cf91af5398b0942ebd
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-05-21 10:56:30 +0200

    Add basic authentication to Main.java (always as servicetester) so that all the dao/service stuff works
    Will be replaced by a login screen in future

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/Main.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/Main.java
@@ -7,11 +7,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring/Spring-Service.xml");
+
+        AuthenticationManager authenticationManager = context.getBean(AuthenticationManager.class);
+        Authentication request = new UsernamePasswordAuthenticationToken("servicetester", "");
+        Authentication result = authenticationManager.authenticate(request);
+        SecurityContextHolder.getContext().setAuthentication(result);
+
         /*
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gui/TablesOverview.fxml"));
         fxmlLoader.setController(new Controller());
