548beda80e3156e75786b1bf3f2f3aa5ffa0fecb
DjConsti <c.brincoveanu@yahoo.com>
2015-06-06 20:13:32 +0200

    add DBAbstractSaleDAO

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/impl/DBIntermittentSaleDAO.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/impl/DBIntermittentSaleDAO.java
@@ -54,8 +54,8 @@ public class DBIntermittentSaleDAO implements DAO<IntermittentSale> {
             throw new DAOException("Inserting sale into database failed", e);
         }
 
-        updateSaleAssoc(intermittentSale);
-        generateSaleHistory(intermittentSale);
+        DBAbstractSaleDAO.updateSaleAssoc(intermittentSale, dataSource, LOGGER);
+        DBAbstractSaleDAO.generateSaleHistory(intermittentSale, dataSource, LOGGER);
 
         //Save IntermittentSale
         final String query = "INSERT INTO IntermittentSale (sale_ID, monday, tuesday, wednesday, thursday, friday, saturday, sunday, fromDayTime, duration, enabled) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
@@ -104,8 +104,8 @@ public class DBIntermittentSaleDAO implements DAO<IntermittentSale> {
             throw new DAOException("Updating sale in database failed", e);
         }
 
-        updateSaleAssoc(intermittentSale);
-        generateSaleHistory(intermittentSale);
+        DBAbstractSaleDAO.updateSaleAssoc(intermittentSale, dataSource, LOGGER);
+        DBAbstractSaleDAO.generateSaleHistory(intermittentSale, dataSource, LOGGER);
 
         //Save IntermittentSale
         final String query = "UPDATE IntermittentSale SET monday = ?, tuesday = ?, wednesday = ?, thursday = ?, friday = ?, saturday = ?, sunday = ?, fromDayTime = ?, duration = ?, enabled = ? WHERE sale_ID = ?";
@@ -156,7 +156,7 @@ public class DBIntermittentSaleDAO implements DAO<IntermittentSale> {
             throw new DAOException("Deleting sale from database failed", e);
         }
 
-        generateSaleHistory(intermittentSale);
+        DBAbstractSaleDAO.generateSaleHistory(intermittentSale, dataSource, LOGGER);
 
         //Delete IntermittentSale
         final String query = "UPDATE IntermittentSale SET deleted = true WHERE sale_ID = ?";
@@ -217,7 +217,7 @@ public class DBIntermittentSaleDAO implements DAO<IntermittentSale> {
             ResultSet result = stmt.executeQuery();
             while (result.next()) {
                 IntermittentSale intermittentSaleFromResult = intermittentSaleFromResultSet(result);
-                addNameToIntermittentSale(intermittentSaleFromResult);
+                DBAbstractSaleDAO.addNameToSale(intermittentSaleFromResult, dataSource);
                 intermittentSales.add(intermittentSaleFromResult);
             }
         } catch (SQLException e) {
@@ -240,7 +240,7 @@ public class DBIntermittentSaleDAO implements DAO<IntermittentSale> {
             ResultSet result = stmt.executeQuery();
             while (result.next()) {
                 IntermittentSale intermittentSaleFromResult = intermittentSaleFromResultSet(result);
-                addNameToIntermittentSale(intermittentSaleFromResult);
+                DBAbstractSaleDAO.addNameToSale(intermittentSaleFromResult, dataSource);
                 intermittentSales.add(intermittentSaleFromResult);
             }
         } catch (SQLException e) {
@@ -305,7 +305,7 @@ public class DBIntermittentSaleDAO implements DAO<IntermittentSale> {
             ResultSet result = stmt.executeQuery();
             while (result.next()) {
                 IntermittentSale intermittentSaleFromResult = intermittentSaleFromResultSet(result);
-                addNameToIntermittentSale(intermittentSaleFromResult);
+                DBAbstractSaleDAO.addNameToSale(intermittentSaleFromResult, dataSource);
                 populatedIntermittentSales.add(intermittentSaleFromResult);
             }
         } catch (SQLException e) {
@@ -389,90 +389,5 @@ public class DBIntermittentSaleDAO implements DAO<IntermittentSale> {
         historyEntry.setData(intermittentSaleFromResultSet(result));
 
         return historyEntry;
-    }
-
-    // ######################### Sale stuff #########################
-
-    /**
-     * writes the changes of the dataset into the database
-     * stores the time; number of the change and the user which executed the changes
-     * @param sale updated dataset
-     * @throws DAOException if an error accessing the database occurred
-     */
-    private void generateSaleHistory(Sale sale) throws DAOException {
-        LOGGER.debug("Entering generateHistory with parameters: " + sale);
-
-        final String query = "INSERT INTO SaleHistory " +
-                "(SELECT *, CURRENT_TIMESTAMP(), ?, " +
-                "(SELECT ISNULL(MAX(changeNr) + 1, 1) FROM SaleHistory WHERE ID = ?) " +
-                "FROM Sale WHERE ID = ?)";
-
-        try (PreparedStatement stmt = dataSource.getConnection().prepareStatement(query)) {
-            stmt.setString(1, SecurityContextHolder.getContext().getAuthentication().getName()); // user
-            stmt.setLong(2, sale.getIdentity());          // dataset id
-            stmt.setLong(3, sale.getIdentity());          // dataset id
-
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            LOGGER.error("generating history failed", e);
-            throw new DAOException("generating history failed", e);
-        }
-    }
-
-    /**
-     * Sets the name of the intermittentSale object according to the value in the database.
-     * @param intermittentSale intermittentSale object from resulting from a query
-     * @throws SQLException if an error accessing the database occurred
-     */
-    private void addNameToIntermittentSale(IntermittentSale intermittentSale) throws SQLException {
-        final String saleQuery = "SELECT name FROM Sale WHERE ID = ?";
-        PreparedStatement stmt2 = dataSource.getConnection().prepareStatement(saleQuery);
-        stmt2.setObject(1, intermittentSale.getIdentity());
-        ResultSet result2 = stmt2.executeQuery();
-        result2.next();
-        String name = result2.getString("name");
-        intermittentSale.setName(name);
-    }
-
-    // ######################### SaleAssoc stuff #########################
-
-    /**
-     * This function is used, to update/create Sale Assoc Entries
-     * All SaleAssoc with the SaleID are disabled, the one, which are used are enabled again and the rest is added
-     * the parameters are used, to identify the Sale Assoc and to set the values
-     *
-     * @param sale Sale Is used to identify the SaleAssoc
-     */
-    private void updateSaleAssoc(Sale sale) throws DAOException {
-        // At first, look if the
-        // disable all SaleAssoc for now, the update sql query will re-enable all valid
-        // associations
-        final String queryDisableSaleAssoc = "UPDATE SaleAssoc SET disabled = true WHERE sale_ID = ?";
-
-        try (PreparedStatement stmt = dataSource.getConnection().prepareStatement(queryDisableSaleAssoc)) {
-            stmt.setLong(1, sale.getIdentity());
-
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            LOGGER.error("Disabling Sale-MenuEntry associations from database failed", e);
-            throw new DAOException("Disabling Sale-MenuEntry associations from database failed", e);
-        }
-
-        final String queryUpdateSaleAssoc =
-                "Merge INTO SaleAssoc (sale_ID, menuEntry_ID, salePrice, disabled) " +
-                        "KEY (sale_ID, menuEntry_ID) VALUES (?, ?, ?, false)";
-
-        try (PreparedStatement stmt = dataSource.getConnection().prepareStatement(queryUpdateSaleAssoc)) {
-            for (MenuEntry entry : sale.getEntries().keySet()) {
-                stmt.setLong(1, sale.getIdentity());
-                stmt.setLong(2, entry.getIdentity());
-                stmt.setBigDecimal(3, sale.getEntries().get(entry));
-                stmt.addBatch();
-            }
-            stmt.executeBatch();
-        } catch (SQLException e) {
-            LOGGER.error("updating saleAssoc in database failed", e);
-            throw new DAOException("updating saleAssoc in database failed", e);
-        }
     }
 }
--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/impl/DBOnetimeSaleDAO.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/impl/DBOnetimeSaleDAO.java
@@ -51,8 +51,8 @@ public class DBOnetimeSaleDAO implements DAO<OnetimeSale> {
             throw new DAOException("Inserting sale into database failed", e);
         }
 
-        updateSaleAssoc(onetimeSale);
-        generateSaleHistory(onetimeSale);
+        DBAbstractSaleDAO.updateSaleAssoc(onetimeSale, dataSource, LOGGER);
+        DBAbstractSaleDAO.generateSaleHistory(onetimeSale, dataSource, LOGGER);
 
         //Create OnetimeSale
         final String query = "INSERT INTO OnetimeSale (sale_ID, fromTime, toTime) VALUES (?, ?, ?)";
@@ -93,8 +93,8 @@ public class DBOnetimeSaleDAO implements DAO<OnetimeSale> {
             throw new DAOException("Updating sale in database failed", e);
         }
 
-        updateSaleAssoc(onetimeSale);
-        generateSaleHistory(onetimeSale);
+        DBAbstractSaleDAO.updateSaleAssoc(onetimeSale, dataSource, LOGGER);
+        DBAbstractSaleDAO.generateSaleHistory(onetimeSale, dataSource, LOGGER);
 
         //Update OnetimeSale
         final String query = "UPDATE OnetimeSale SET fromTime = ?, toTime = ? WHERE sale_ID = ?";
@@ -137,7 +137,7 @@ public class DBOnetimeSaleDAO implements DAO<OnetimeSale> {
             throw new DAOException("Deleting sale from database failed", e);
         }
 
-        generateSaleHistory(onetimeSale);
+        DBAbstractSaleDAO.generateSaleHistory(onetimeSale, dataSource, LOGGER);
 
         //Delete OnetimeSale
         final String query = "UPDATE OnetimeSale SET deleted = true WHERE sale_ID = ?";
@@ -182,7 +182,7 @@ public class DBOnetimeSaleDAO implements DAO<OnetimeSale> {
             ResultSet result = stmt.executeQuery();
             while (result.next()) {
                 OnetimeSale onetimeSaleFromResult = onetimeSaleFromResultSet(result);
-                addNameToOnetimeSale(onetimeSaleFromResult);
+                DBAbstractSaleDAO.addNameToSale(onetimeSaleFromResult, dataSource);
                 onetimeSales.add(onetimeSaleFromResult);
             }
         } catch (SQLException e) {
@@ -205,7 +205,7 @@ public class DBOnetimeSaleDAO implements DAO<OnetimeSale> {
             ResultSet result = stmt.executeQuery();
             while (result.next()) {
                 OnetimeSale onetimeSaleFromResult = onetimeSaleFromResultSet(result);
-                addNameToOnetimeSale(onetimeSaleFromResult);
+                DBAbstractSaleDAO.addNameToSale(onetimeSaleFromResult, dataSource);
                 onetimeSales.add(onetimeSaleFromResult);
             }
         } catch (SQLException e) {
@@ -269,7 +269,7 @@ public class DBOnetimeSaleDAO implements DAO<OnetimeSale> {
             ResultSet result = stmt.executeQuery();
             while (result.next()) {
                 OnetimeSale onetimeSaleFromResult = onetimeSaleFromResultSet(result);
-                addNameToOnetimeSale(onetimeSaleFromResult);
+                DBAbstractSaleDAO.addNameToSale(onetimeSaleFromResult, dataSource);
                 populatedOnetimeSales.add(onetimeSaleFromResult);
             }
         } catch (SQLException e) {
@@ -344,90 +344,5 @@ public class DBOnetimeSaleDAO implements DAO<OnetimeSale> {
         historyEntry.setData(onetimeSaleFromResultSet(result));
 
         return historyEntry;
-    }
-
-    // ######################### Sale stuff #########################
-
-    /**
-     * writes the changes of the dataset into the database
-     * stores the time; number of the change and the user which executed the changes
-     * @param sale updated dataset
-     * @throws DAOException if an error accessing the database occurred
-     */
-    private void generateSaleHistory(Sale sale) throws DAOException {
-        LOGGER.debug("Entering generateHistory with parameters: " + sale);
-
-        final String query = "INSERT INTO SaleHistory " +
-                "(SELECT *, CURRENT_TIMESTAMP(), ?, " +
-                "(SELECT ISNULL(MAX(changeNr) + 1, 1) FROM SaleHistory WHERE ID = ?) " +
-                "FROM Sale WHERE ID = ?)";
-
-        try (PreparedStatement stmt = dataSource.getConnection().prepareStatement(query)) {
-            stmt.setString(1, SecurityContextHolder.getContext().getAuthentication().getName()); // user
-            stmt.setLong(2, sale.getIdentity());          // dataset id
-            stmt.setLong(3, sale.getIdentity());          // dataset id
-
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            LOGGER.error("generating history failed", e);
-            throw new DAOException("generating history failed", e);
-        }
-    }
-
-    /**
-     * Sets the name of the onetimesale object according to the value in the database.
-     * @param onetimeSale onetimesale object from resulting from a query
-     * @throws SQLException if an error accessing the database occurred
-     */
-    private void addNameToOnetimeSale(OnetimeSale onetimeSale) throws SQLException {
-        final String saleQuery = "SELECT name FROM Sale WHERE ID = ?";
-        PreparedStatement stmt2 = dataSource.getConnection().prepareStatement(saleQuery);
-        stmt2.setObject(1, onetimeSale.getIdentity());
-        ResultSet result2 = stmt2.executeQuery();
-        result2.next();
-        String name = result2.getString("name");
-        onetimeSale.setName(name);
-    }
-
-    // ######################### SaleAssoc stuff #########################
-
-    /**
-     * This function is used, to update/create Sale Assoc Entries
-     * All SaleAssoc with the SaleID are disabled, the one, which are used are enabled again and the rest is added
-     * the parameters are used, to identify the Sale Assoc and to set the values
-     *
-     * @param sale Sale Is used to identify the SaleAssoc
-     */
-    private void updateSaleAssoc(Sale sale) throws DAOException {
-        // At first, look if the
-        // disable all SaleAssoc for now, the update sql query will re-enable all valid
-        // associations
-        final String queryDisableSaleAssoc = "UPDATE SaleAssoc SET disabled = true WHERE sale_ID = ?";
-
-        try (PreparedStatement stmt = dataSource.getConnection().prepareStatement(queryDisableSaleAssoc)) {
-            stmt.setLong(1, sale.getIdentity());
-
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            LOGGER.error("Disabling Sale-MenuEntry associations from database failed", e);
-            throw new DAOException("Disabling Sale-MenuEntry associations from database failed", e);
-        }
-
-        final String queryUpdateSaleAssoc =
-                "Merge INTO SaleAssoc (sale_ID, menuEntry_ID, salePrice, disabled) " +
-                        "KEY (sale_ID, menuEntry_ID) VALUES (?, ?, ?, false)";
-
-        try (PreparedStatement stmt = dataSource.getConnection().prepareStatement(queryUpdateSaleAssoc)) {
-            for (MenuEntry entry : sale.getEntries().keySet()) {
-                stmt.setLong(1, sale.getIdentity());
-                stmt.setLong(2, entry.getIdentity());
-                stmt.setBigDecimal(3, sale.getEntries().get(entry));
-                stmt.addBatch();
-            }
-            stmt.executeBatch();
-        } catch (SQLException e) {
-            LOGGER.error("updating saleAssoc in database failed", e);
-            throw new DAOException("updating saleAssoc in database failed", e);
-        }
     }
 }
