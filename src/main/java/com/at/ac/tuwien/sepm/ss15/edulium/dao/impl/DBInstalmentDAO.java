c5fdbb445c568f3cdb1e6f6951c52f828cb4102b
Philippos Gorgoris <philipgorgoris@gmail.com>
2015-06-08 03:00:14 +0200

    Implemented the create method in InstalmentDAO

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/impl/DBInstalmentDAO.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/dao/impl/DBInstalmentDAO.java
@@ -5,7 +5,13 @@ import com.at.ac.tuwien.sepm.ss15.edulium.dao.DAOException;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.Instalment;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.history.History;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.ValidationException;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.Validator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.List;
 
 /**
@@ -13,9 +19,46 @@ import java.util.List;
  */
 class DBInstalmentDAO implements DAO<Instalment> {
 
+    private static final Logger LOGGER = LogManager.getLogger(DBInstalmentDAO.class);
+
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private Validator<Instalment> instalmentValidator;
+
+    /**
+     * Writes the instalment object into the database and sets the identity
+     * parameter that was generated there
+     */
     @Override
     public void create(Instalment object) throws DAOException, ValidationException {
+        LOGGER.debug("Entering create with parameters: " + object);
 
+        instalmentValidator.validateForCreate(object);
+
+        final String query = "INSERT INTO Instalment (instalmentTime, paymentInfo, type, " +
+                "amount, invoice_ID) VALUES (?, ?, ?, ?, ?)";
+
+        try (PreparedStatement stmt = dataSource.getConnection().prepareStatement(query,
+                Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setTimestamp(1, Timestamp.valueOf(object.getTime()));
+            stmt.setString(2, object.getPaymentInfo());
+            stmt.setString(3, object.getType());
+            stmt.setBigDecimal(4, object.getAmount());
+            stmt.setLong(5, object.getInvoice().getIdentity());
+            stmt.executeUpdate();
+
+            ResultSet key = stmt.getGeneratedKeys();
+            if (key.next()) {
+                object.setIdentity(key.getLong(1));
+            }
+            key.close();
+        } catch (SQLException e) {
+            LOGGER.error("Failed to insert instalment entry into database", e);
+            throw new DAOException("Failed to insert instalment entry into database", e);
+        }
+
+        // TODO: Generate history
     }
 
     @Override
