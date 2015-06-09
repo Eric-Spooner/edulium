eb1cca984d1c08ead134e8ec87bd5af18ef82746
Philipp Paris <paris.philipp@gmail.com>
2015-06-09 11:11:10 +0200

    reset changes in Main.java

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/Main.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/Main.java
@@ -1,21 +1,14 @@
 package com.at.ac.tuwien.sepm.ss15.edulium;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
-
-        Scene scene = new Scene(root, 300, 275);
-
-        primaryStage.setTitle("FXML Welcome");
-        primaryStage.setScene(scene);
+        primaryStage.setWidth(300);
+        primaryStage.setHeight(300);
+        primaryStage.setTitle("Edulium");
         primaryStage.show();
     }
 }
