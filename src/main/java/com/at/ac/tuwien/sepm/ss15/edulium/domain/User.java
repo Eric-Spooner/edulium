267653ee7d16806f07e7b6b38b5d6b05be0e58d1
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-06-10 23:23:34 +0200

    add hashCode methods to all domains

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/User.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/User.java
@@ -85,4 +85,12 @@ public class User {
         return !(role != null ? !role.equals(user.role) : user.role != null);
 
     }
+
+    @Override
+    public int hashCode() {
+        int result = identity != null ? identity.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        return result;
+    }
 }
