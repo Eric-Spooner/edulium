98c788aaafd040ba478de0f32182f8afd7ea15cf
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-06-16 19:13:33 +0200

    add a menu category overview ui component

---
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/gui/MenuCategoryOverviewController.java
@@ -0,0 +1,109 @@
+package com.at.ac.tuwien.sepm.ss15.edulium.gui;
+
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.MenuCategory;
+import com.at.ac.tuwien.sepm.ss15.edulium.service.MenuService;
+import com.at.ac.tuwien.sepm.ss15.edulium.service.ServiceException;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.controlsfx.control.GridCell;
+import org.controlsfx.control.GridView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.TaskScheduler;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class MenuCategoryOverviewController implements Initializable, Controller {
+    private static final Logger LOGGER = LogManager.getLogger(MenuCategoryOverviewController.class);
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @Autowired
+    private MenuService menuService;
+    @Autowired
+    private TaskScheduler taskScheduler;
+
+    private PollingList<MenuCategory> menuCategories;
+    private SortedList<MenuCategory> sortedMenuCategories;
+
+    private Consumer<MenuCategory> menuCategoryClickedConsumer = null;
+
+    private class UserButtonCell extends GridCell<MenuCategory> {
+        private Button button = new Button();
+        private MenuCategory menuCategory = null;
+
+        public UserButtonCell() {
+            button.setPrefSize(240, 100);
+            button.setStyle("-fx-font-size: 18px;");
+            button.setOnAction(e -> onMenuCategoryClicked(menuCategory));
+
+            setGraphic(button);
+        }
+
+        @Override
+        public void updateIndex(int i) {
+            if (i >= 0) {
+                menuCategory = sortedMenuCategories.get(i);
+                button.setText(menuCategory.getName());
+            }
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        menuCategories = new PollingList<>(taskScheduler);
+        menuCategories.setInterval(1000);
+        menuCategories.setSupplier(new Supplier<List<MenuCategory>>() {
+            @Override
+            public List<MenuCategory> get() {
+                try {
+                    return menuService.getAllMenuCategories();
+                } catch (ServiceException e) {
+                    LOGGER.error("Getting all menu categories via menu category supplier has failed", e);
+                    return null;
+                }
+            }
+        });
+
+        sortedMenuCategories = new SortedList<>(menuCategories);
+        sortedMenuCategories.setComparator((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+
+        GridView<MenuCategory> gridView = new GridView<>(sortedMenuCategories);
+        gridView.setCellFactory(view -> new UserButtonCell());
+        gridView.setCellWidth(240);
+        gridView.setCellHeight(100);
+
+        scrollPane.setContent(gridView);
+        scrollPane.setStyle("-fx-font-size: 40px;");
+    }
+
+    public void setOnMenuCategoryClicked(Consumer<MenuCategory> menuCategoryClickedConsumer)
+    {
+        this.menuCategoryClickedConsumer = menuCategoryClickedConsumer;
+    }
+
+    private void onMenuCategoryClicked(MenuCategory menuCategory)
+    {
+        if (menuCategoryClickedConsumer != null) {
+            menuCategoryClickedConsumer.accept(menuCategory);
+        }
+    }
+
+    @Override
+    public void disable(boolean disabled) {
+        if (disabled) {
+            menuCategories.stopPolling();
+        } else {
+            menuCategories.startPolling();
+        }
+    }
+}
