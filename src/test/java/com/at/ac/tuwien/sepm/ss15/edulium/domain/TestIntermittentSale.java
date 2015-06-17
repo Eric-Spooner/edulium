d55ee11b60d9c8d9fd2475f9325653787204377a
Constantin Brincoveanu <c.brincoveanu@yahoo.com>
2015-06-17 20:34:04 +0200

    improve tests

--- src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/TestIntermittentSale.java
+++ src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/TestIntermittentSale.java
@@ -29,4 +29,144 @@ public class TestIntermittentSale {
 
         assertTrue(intermittentSale1.isAt(time2));
     }
+
+    @Test
+    public void testIsAt_shouldNotAcceptWrongDay1() {
+        LocalDateTime time1 = LocalDateTime.parse("2015-06-15T10:15:30");
+        LocalDateTime time2 = LocalDateTime.parse("2015-06-15T10:15:30");
+
+        IntermittentSale intermittentSale1 = new IntermittentSale();
+        intermittentSale1.setIdentity(new Long(123));
+        intermittentSale1.setDuration(60);
+        intermittentSale1.setMonday(false);
+        intermittentSale1.setTuesday(false);
+        intermittentSale1.setWednesday(false);
+        intermittentSale1.setThursday(false);
+        intermittentSale1.setFriday(false);
+        intermittentSale1.setSaturday(false);
+        intermittentSale1.setSunday(false);
+        intermittentSale1.setFromDayTime(time1);
+
+        assertFalse(intermittentSale1.isAt(time2));
+    }
+
+    @Test
+    public void testIsAt_shouldNotAcceptWrongDay2() {
+        LocalDateTime time1 = LocalDateTime.parse("2015-06-16T10:15:30");
+        LocalDateTime time2 = LocalDateTime.parse("2015-06-16T10:15:30");
+
+        IntermittentSale intermittentSale1 = new IntermittentSale();
+        intermittentSale1.setIdentity(new Long(123));
+        intermittentSale1.setDuration(60);
+        intermittentSale1.setMonday(false);
+        intermittentSale1.setTuesday(false);
+        intermittentSale1.setWednesday(false);
+        intermittentSale1.setThursday(false);
+        intermittentSale1.setFriday(false);
+        intermittentSale1.setSaturday(false);
+        intermittentSale1.setSunday(false);
+        intermittentSale1.setFromDayTime(time1);
+
+        assertFalse(intermittentSale1.isAt(time2));
+    }
+
+    @Test
+    public void testIsAt_shouldNotAcceptWrongDay3() {
+        LocalDateTime time1 = LocalDateTime.parse("2015-06-17T10:15:30");
+        LocalDateTime time2 = LocalDateTime.parse("2015-06-17T10:15:30");
+
+        IntermittentSale intermittentSale1 = new IntermittentSale();
+        intermittentSale1.setIdentity(new Long(123));
+        intermittentSale1.setDuration(60);
+        intermittentSale1.setMonday(false);
+        intermittentSale1.setTuesday(false);
+        intermittentSale1.setWednesday(false);
+        intermittentSale1.setThursday(false);
+        intermittentSale1.setFriday(false);
+        intermittentSale1.setSaturday(false);
+        intermittentSale1.setSunday(false);
+        intermittentSale1.setFromDayTime(time1);
+
+        assertFalse(intermittentSale1.isAt(time2));
+    }
+
+    @Test
+    public void testIsAt_shouldNotAcceptWrongDay4() {
+        LocalDateTime time1 = LocalDateTime.parse("2015-06-18T10:15:30");
+        LocalDateTime time2 = LocalDateTime.parse("2015-06-18T10:15:30");
+
+        IntermittentSale intermittentSale1 = new IntermittentSale();
+        intermittentSale1.setIdentity(new Long(123));
+        intermittentSale1.setDuration(60);
+        intermittentSale1.setMonday(false);
+        intermittentSale1.setTuesday(false);
+        intermittentSale1.setWednesday(false);
+        intermittentSale1.setThursday(false);
+        intermittentSale1.setFriday(false);
+        intermittentSale1.setSaturday(false);
+        intermittentSale1.setSunday(false);
+        intermittentSale1.setFromDayTime(time1);
+
+        assertFalse(intermittentSale1.isAt(time2));
+    }
+
+    @Test
+    public void testIsAt_shouldNotAcceptWrongDay5() {
+        LocalDateTime time1 = LocalDateTime.parse("2015-06-19T10:15:30");
+        LocalDateTime time2 = LocalDateTime.parse("2015-06-19T10:15:30");
+
+        IntermittentSale intermittentSale1 = new IntermittentSale();
+        intermittentSale1.setIdentity(new Long(123));
+        intermittentSale1.setDuration(60);
+        intermittentSale1.setMonday(false);
+        intermittentSale1.setTuesday(false);
+        intermittentSale1.setWednesday(false);
+        intermittentSale1.setThursday(false);
+        intermittentSale1.setFriday(false);
+        intermittentSale1.setSaturday(false);
+        intermittentSale1.setSunday(false);
+        intermittentSale1.setFromDayTime(time1);
+
+        assertFalse(intermittentSale1.isAt(time2));
+    }
+
+    @Test
+    public void testIsAt_shouldNotAcceptWrongDay6() {
+        LocalDateTime time1 = LocalDateTime.parse("2015-06-20T10:15:30");
+        LocalDateTime time2 = LocalDateTime.parse("2015-06-20T10:15:30");
+
+        IntermittentSale intermittentSale1 = new IntermittentSale();
+        intermittentSale1.setIdentity(new Long(123));
+        intermittentSale1.setDuration(60);
+        intermittentSale1.setMonday(false);
+        intermittentSale1.setTuesday(false);
+        intermittentSale1.setWednesday(false);
+        intermittentSale1.setThursday(false);
+        intermittentSale1.setFriday(false);
+        intermittentSale1.setSaturday(false);
+        intermittentSale1.setSunday(false);
+        intermittentSale1.setFromDayTime(time1);
+
+        assertFalse(intermittentSale1.isAt(time2));
+    }
+
+    @Test
+    public void testIsAt_shouldNotAcceptWrongDay7() {
+        LocalDateTime time1 = LocalDateTime.parse("2015-06-21T10:15:30");
+        LocalDateTime time2 = LocalDateTime.parse("2015-06-21T10:15:30");
+
+        IntermittentSale intermittentSale1 = new IntermittentSale();
+        intermittentSale1.setIdentity(new Long(123));
+        intermittentSale1.setDuration(60);
+        intermittentSale1.setMonday(false);
+        intermittentSale1.setTuesday(false);
+        intermittentSale1.setWednesday(false);
+        intermittentSale1.setThursday(false);
+        intermittentSale1.setFriday(false);
+        intermittentSale1.setSaturday(false);
+        intermittentSale1.setSunday(false);
+        intermittentSale1.setFromDayTime(time1);
+
+        assertFalse(intermittentSale1.isAt(time2));
+    }
 }
--- src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/TestOnetimeSale.java
+++ src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/TestOnetimeSale.java
@@ -23,4 +23,32 @@ public class TestOnetimeSale {
 
         assertTrue(onetimeSale.isAt(time3));
     }
+
+    @Test
+    public void testIsAt_shouldNotAcceptBefore() {
+        LocalDateTime time1 = LocalDateTime.parse("2015-06-17T10:15:30");
+        LocalDateTime time2 = LocalDateTime.parse("2015-06-17T10:16:30");
+        LocalDateTime time3 = LocalDateTime.parse("2015-06-17T10:14:00");
+
+        OnetimeSale onetimeSale = new OnetimeSale();
+        onetimeSale.setIdentity(new Long(123));
+        onetimeSale.setFromTime(time1);
+        onetimeSale.setToTime(time2);
+
+        assertFalse(onetimeSale.isAt(time3));
+    }
+
+    @Test
+    public void testIsAt_shouldNotAcceptAfter() {
+        LocalDateTime time1 = LocalDateTime.parse("2015-06-17T10:15:30");
+        LocalDateTime time2 = LocalDateTime.parse("2015-06-17T10:16:30");
+        LocalDateTime time3 = LocalDateTime.parse("2015-06-17T10:18:00");
+
+        OnetimeSale onetimeSale = new OnetimeSale();
+        onetimeSale.setIdentity(new Long(123));
+        onetimeSale.setFromTime(time1);
+        onetimeSale.setToTime(time2);
+
+        assertFalse(onetimeSale.isAt(time3));
+    }
 }
