60d445ce21224a80d06cd5b311eae354e297a0b7
Constantin Brincoveanu <c.brincoveanu@yahoo.com>
2015-06-17 18:08:06 +0200

    add domain classes

---
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/domain/Sale.java
@@ -0,0 +1,69 @@
+package com.at.ac.tuwien.sepm.ss15.edulium.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Hashtable;
+
+/**
+ * Abstract domain object representing a Sale
+ */
+public abstract class Sale {
+    protected Long identity = null;
+    protected String name = null;
+    protected Hashtable<MenuEntry, BigDecimal> entries = null;
+
+    /**
+     * @return the identity of the sale (can be null)
+     */
+    public Long getIdentity() {
+        return identity;
+    }
+
+    /**
+     * Sets the identity of the sale
+     * @param identity identity of the sale
+     */
+    public void setIdentity(Long identity) {
+        this.identity = identity;
+    }
+
+    /**
+     * @return the name of the sale (can be null)
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the sale
+     * @param name name of the sale
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @param time the time that has to be checked
+     * @return true if the Sale is active at the given time
+     */
+    public abstract boolean isAt(LocalDateTime time);
+
+
+    /**
+     *
+     * @return the entries of the sale with the new price
+     */
+    public Hashtable<MenuEntry, BigDecimal> getEntries() {
+        return entries;
+    }
+
+
+    /**
+     * sets the entries/price list of the sale
+     * @param entries the entries list
+     */
+    public void setEntries(Hashtable<MenuEntry, BigDecimal> entries) {
+        this.entries = entries;
+    }
+}
