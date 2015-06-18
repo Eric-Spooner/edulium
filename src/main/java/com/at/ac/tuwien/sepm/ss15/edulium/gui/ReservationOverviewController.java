f4dc1905d76155b6e7046018f12f50e51e8ef042
Philipp Paris <paris.philipp@gmail.com>
2015-06-18 13:45:37 +0200

    reservationEditView: removed auto create button

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/gui/ReservationOverviewController.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/gui/ReservationOverviewController.java
@@ -156,7 +156,7 @@ public class ReservationOverviewController implements Initializable, Controller 
 
         lvReservations.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             // set edit button disabled if reservation is bygone
-            boolean disable = newValue.getTime().plus(newValue.getDuration()).isBefore(LocalDateTime.now());
+            boolean disable = newValue == null? null : newValue.getTime().plus(newValue.getDuration()).isBefore(LocalDateTime.now());
             btnEdit.setDisable(disable);
             btnDelete.setDisable(disable);
         });
