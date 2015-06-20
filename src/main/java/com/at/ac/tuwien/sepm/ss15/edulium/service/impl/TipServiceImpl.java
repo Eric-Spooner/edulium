0da337d8b855a06143abe891792ba277e3723879
Eric.Moerth <e1325306@student.tuwien.ac.at>
2015-06-21 01:10:07 +0200

    Little Changes

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/service/impl/TipServiceImpl.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/service/impl/TipServiceImpl.java
@@ -2,18 +2,23 @@ package com.at.ac.tuwien.sepm.ss15.edulium.service.impl;
 
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.Invoice;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.Order;
+import com.at.ac.tuwien.sepm.ss15.edulium.service.OrderService;
 import com.at.ac.tuwien.sepm.ss15.edulium.service.TipService;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * The Implementation of the Tip-Service
  */
 class TipServiceImpl implements TipService {
+    @Resource(name = "orderService")
+    private OrderService orderService;
+
     @Override
     public void calculateTheTipAndMatchItToUser(Invoice invoice) {
         for(Order order: invoice.getOrders()){
-
+            
         }
     }
 }
