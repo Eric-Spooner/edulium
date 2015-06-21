79549fcb1e7deb8214bac3e41da861e30e75f6a9
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-06-21 16:53:13 +0200

    fix testdata.sql user

--- src/main/resources/database/testdata.sql
+++ src/main/resources/database/testdata.sql
@@ -1,6 +1,6 @@
-MERGE INTO RestaurantUser (ID, name, userRole) KEY(ID) VALUES ('cook', 'Crusty Cook', 'ROLE_COOK');
-MERGE INTO RestaurantUser (ID, name, userRole) KEY(ID) VALUES ('waiter', 'Wily Waiter', 'ROLE_SERVICE');
-MERGE INTO RestaurantUser (ID, name, userRole) KEY(ID) VALUES ('manager', 'Maverick Manager', 'ROLE_MANAGER');
+MERGE INTO RestaurantUser (ID, name, userRole, tip) KEY(ID) VALUES ('cook', 'Crusty Cook', 'ROLE_COOK', 0);
+MERGE INTO RestaurantUser (ID, name, userRole, tip) KEY(ID) VALUES ('waiter', 'Wily Waiter', 'ROLE_SERVICE', 0);
+MERGE INTO RestaurantUser (ID, name, userRole, tip) KEY(ID) VALUES ('manager', 'Maverick Manager', 'ROLE_MANAGER', 0);
 
 MERGE INTO MenuCategory (ID, name) KEY(ID) VALUES (1, 'starter');
 MERGE INTO MenuCategory (ID, name) KEY(ID) VALUES (2, 'main dish');
