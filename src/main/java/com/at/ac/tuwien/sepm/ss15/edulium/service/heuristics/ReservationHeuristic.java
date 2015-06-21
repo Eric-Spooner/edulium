5f873b79554b6a66f0115c224e89b88454e54555
Philipp Paris <paris.philipp@gmail.com>
2015-06-21 18:41:29 +0200

    ReservationService added findTablesForReservation(res) method addReservation / updateReservation do not add tables automatically

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/service/heuristics/ReservationHeuristic.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/service/heuristics/ReservationHeuristic.java
@@ -3,15 +3,30 @@ package com.at.ac.tuwien.sepm.ss15.edulium.service.heuristics;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.Reservation;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.Table;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.ValidationException;
+import com.at.ac.tuwien.sepm.ss15.edulium.service.ReservationService;
 import com.at.ac.tuwien.sepm.ss15.edulium.service.ServiceException;
 
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
  * Reservation Heuristics Interface
  */
-public interface ReservationHeuristic {
+public abstract class ReservationHeuristic {
+    @Resource(name = "reservationService")
+    private ReservationService reservationService;
 
-    List<Table> getTablesForReservation(Reservation reservation, List<Table> tables) throws ValidationException, ServiceException;
+    public abstract List<Table> getTablesForReservation(Reservation reservation, List<Table> tables) throws ValidationException, ServiceException;
+
+    protected List<Reservation> getOverlappingReservations(Reservation reservation) throws ServiceException, ValidationException {
+        List<Reservation> reservations = reservationService.findReservationBetween(reservation.getTime(),
+                reservation.getTime().plus(reservation.getDuration()));
+
+        // ignore current reservation
+        reservations.removeIf(res -> res.getIdentity().equals(reservation.getIdentity()));
+
+        return reservations;
+    }
 
 }
