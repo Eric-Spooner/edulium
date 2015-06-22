40391af1a53d6deeee33d4259da2ce5546eecb69
Philipp Paris <paris.philipp@gmail.com>
2015-06-23 01:08:30 +0200

    updated manager gui: menu

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/gui/MenuViewController.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/gui/MenuViewController.java
@@ -1,6 +1,8 @@
 package com.at.ac.tuwien.sepm.ss15.edulium.gui;
 
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.Menu;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.ValidationException;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.Validator;
 import com.at.ac.tuwien.sepm.ss15.edulium.service.MenuService;
 import com.at.ac.tuwien.sepm.ss15.edulium.service.ServiceException;
 import javafx.beans.property.SimpleStringProperty;
@@ -39,129 +41,154 @@ public class MenuViewController implements Initializable {
     private TableColumn<Menu,String> tableColMenuName;
     @FXML
     private TableColumn<Menu,String> tableColMenuEntries;
+    @FXML
+    private Button buttonMenuUpdate;
+    @FXML
+    private Button buttonMenuRemove;
+
+    @Autowired
+    private Validator<Menu> menuValidator;
 
     @Resource(name = "menuDialogPane")
     private FXMLPane menuDialogPane;
-
     private MenuDialogController menuDialogController;
-    private Dialog<Menu> menuDialog;
 
     @Autowired
     private MenuService menuService;
-    @Autowired
-    private TaskScheduler taskScheduler;
 
     private ObservableList<Menu> menus;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        menuDialogController = menuDialogPane.getController(MenuDialogController.class);
+
         // queued
         try {
             menus = observableArrayList(menuService.getAllMenus());
             tableViewMenu.setItems(menus);
-            tableColMenuId.setCellValueFactory(new PropertyValueFactory<Menu, Long>("identity"));
-            tableColMenuName.setCellValueFactory(new PropertyValueFactory<Menu, String>("name"));
-            tableColMenuEntries.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Menu, String>, ObservableValue<String>>() {
-                public ObservableValue<String> call(TableColumn.CellDataFeatures<Menu, String> p) {
-                    // p.getValue() returns the Person instance for a particular TableView row
-                    List<String> list = new LinkedList<String>();
-                    p.getValue().getEntries().forEach(entry->list.add(entry.getName()));
-                    return new SimpleStringProperty(list.toString());
-                }
-            });
-
-            menuDialogController = menuDialogPane.getController(MenuDialogController.class);
-            menuDialog = new Dialog<>();
-            menuDialog.getDialogPane().setContent(menuDialogPane);
-            ButtonType cancelButtonType = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
-            menuDialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, cancelButtonType);
-            final Button btOk = (Button) menuDialog.getDialogPane().lookupButton(ButtonType.OK);
-            btOk.addEventFilter(ActionEvent.ACTION, event -> {
-                if (!menuDialogController.validateData()) {
-                    event.consume();
-                }
-            });
-            final Button cancelButton = (Button) menuDialog.getDialogPane().lookupButton(cancelButtonType);
-            cancelButton.addEventFilter(ActionEvent.ACTION, event -> {
-                menuDialogController.resetDialog();
-            });
         }catch (ServiceException e){
             LOGGER.error("Initialize Menu View Failed due to" + e);
         }
 
+        tableColMenuId.setCellValueFactory(new PropertyValueFactory<Menu, Long>("identity"));
+        tableColMenuName.setCellValueFactory(new PropertyValueFactory<Menu, String>("name"));
+        tableColMenuEntries.setCellValueFactory(p -> {
+            // p.getValue() returns the Person instance for a particular TableView row
+            List<String> list = new LinkedList<String>();
+            p.getValue().getEntries().forEach(entry->list.add(entry.getName()));
+            return new SimpleStringProperty(list.toString());
+        });
     }
 
+    @FXML
     public void buttonMenuUpdateClicked(ActionEvent actionEvent) {
-        try {
-            LOGGER.info("Update Menu Button Click");
-            Stage stage = new Stage();
-            if(tableViewMenu.getSelectionModel().getSelectedItem() == null){
-                ManagerViewController.showErrorDialog
-                        ("Error", "Input Validation Error", "You have to select a Menu to Update");
-                return;
-            }
-            menuDialogController.resetDialog();
-            menuDialogController.setDialogEnumeration(DialogEnumeration.UPDATE);
-            menuDialogController.setMenu(tableViewMenu.getSelectionModel().getSelectedItem());
-            menuDialogController.showMenu();
-            menuDialog.showAndWait();
-            menus.setAll(menuService.getAllMenus());
-            menuDialogController.resetDialog();
-        }catch (Exception e){
-            LOGGER.error("Loading the Menus failed" + e);
+        Menu selectedMenu = tableViewMenu.getSelectionModel().getSelectedItem();
+        if (selectedMenu != null) {
+            UpdateInputDialog<Menu> menuInputDialog = new UpdateInputDialog<>("menu", selectedMenu);
+            menuInputDialog.setValidator(menuValidator);
+            menuInputDialog.setContent(menuDialogPane);
+            menuInputDialog.setController(menuDialogController);
+            menuInputDialog.showAndWait().ifPresent(editedMenu -> {
+                try {
+                    menuService.updateMenu(editedMenu);
+                    menus.remove(selectedMenu);
+                    menus.add(editedMenu);
+                } catch (ValidationException | ServiceException e) {
+                    LOGGER.error("Could not change menu category " + editedMenu, e);
+
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error while updating menu category");
+                    alert.setHeaderText("Could not update menu category '" + selectedMenu.getName() + "'");
+                    alert.setContentText(e.getMessage());
+                    alert.showAndWait();
+                }
+            });
         }
     }
 
+    @FXML
     public void buttonMenuSearchClicked(ActionEvent actionEvent) {
-        try {
-            LOGGER.info("Search Menu Button Click");
-            LOGGER.info("Add Menu Button Click");
-            menuDialogController.resetDialog();
-            menuDialogController.setDialogEnumeration(DialogEnumeration.SEARCH);
-            menuDialog.showAndWait();
-            if(menuDialogController.getMenu() != null){
-                menus.setAll(menuService.findMenu(menuDialogController.getMenu()));
-            }else {
-                menus.setAll(menuService.getAllMenus());
+        SearchInputDialog<Menu> menuSearchDialog = new SearchInputDialog<>("menus");
+        menuSearchDialog.setContent(menuDialogPane);
+        menuSearchDialog.setController(menuDialogController);
+        menuSearchDialog.showAndWait().ifPresent(menuMatcher -> {
+            try {
+                menus.setAll(menuService.findMenu(menuMatcher));
+            } catch (ServiceException e) {
+                LOGGER.error("Could not search for menus with matcher " + menuMatcher, e);
+
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error while searching for menus");
+                alert.setHeaderText("Could not search for menus");
+                alert.setContentText(e.getMessage());
+                alert.showAndWait();
             }
-            menuDialogController.resetDialog();
-        }catch (ServiceException e){
-            LOGGER.error("Menu Service finding Menus did not work" + e);
-        }
+        });
     }
 
+    @FXML
     public void buttonMenuRemoveClicked(ActionEvent actionEvent) {
-        try {
-            LOGGER.info("Delete Menu Button Click");
-            if(tableViewMenu.getSelectionModel().getSelectedItem() == null){
-                ManagerViewController.showErrorDialog
-                        ("Error", "Input Validation Error", "You have to select a Menu to Delete");
-                return;
+        Menu selectedMenu = tableViewMenu.getSelectionModel().getSelectedItem();
+
+        if(selectedMenu != null) {
+            try {
+                menuService.removeMenu(selectedMenu);
+                menus.remove(selectedMenu);
+            } catch (ValidationException | ServiceException e) {
+                LOGGER.error("Could not remove menu entry " + selectedMenu, e);
+
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error while removing menus");
+                alert.setHeaderText("Could not removing menus");
+                alert.setContentText(e.getMessage());
+                alert.showAndWait();
             }
-            menuService.removeMenu(tableViewMenu.getSelectionModel().getSelectedItem());
-            menus.setAll(menuService.getAllMenus());
-        }catch (Exception e){
-            LOGGER.error("Loading the Menus failed" + e);
         }
     }
 
+    @FXML
     public void buttonMenuAddClicked(ActionEvent actionEvent){
-        try {
-            LOGGER.info("Add Menu Button Click");
-            menuDialogController.resetDialog();
-            menuDialogController.setDialogEnumeration(DialogEnumeration.ADD);
-            menuDialog.showAndWait();
-            menus.setAll(menuService.getAllMenus());
-        }catch (Exception e){
-            LOGGER.error("Loading the Menus failed" + e);
-        }
+        CreateInputDialog<Menu> menuInputDialog = new CreateInputDialog<>("menu");
+        menuInputDialog.setValidator(menuValidator);
+        menuInputDialog.setContent(menuDialogPane);
+        menuInputDialog.setController(menuDialogController);
+        menuInputDialog.showAndWait().ifPresent(menu -> {
+            try {
+                menuService.addMenu(menu);
+                menus.add(menu);
+            } catch (ValidationException | ServiceException e) {
+                LOGGER.error("Could not add menu " + menu, e);
+
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error while adding menu");
+                alert.setHeaderText("Could not add menu '" + menu.getName() + "'");
+                alert.setContentText(e.getMessage());
+                alert.showAndWait();
+            }
+        });
     }
 
-    public void buttonShowAllMenuClicked(ActionEvent actionEvent) {
+    @FXML
+    public void on_tableView_clicked() {
+        boolean disable = tableViewMenu.getSelectionModel().isEmpty();
+
+        buttonMenuRemove.setDisable(disable);
+        buttonMenuUpdate.setDisable(disable);
+    }
+
+    @FXML
+    public void buttonShowAllClicked() {
+        menus.clear();
         try {
-            menus.setAll(menuService.getAllMenus());
-        } catch (Exception e){
-            LOGGER.error("Loading All Menu failed" + e);
+            menus.addAll(menuService.getAllMenus());
+        } catch (ServiceException e) {
+            LOGGER.error("Could not show menus ", e);
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error while loading menus");
+            alert.setHeaderText("Could not load menus");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
         }
     }
 }
