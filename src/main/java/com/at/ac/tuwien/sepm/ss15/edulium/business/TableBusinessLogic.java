054d031009f2386bdceb53e1913ac61160fd08cb
Eric.Moerth <e1325306@student.tuwien.ac.at>
2015-06-23 21:24:21 +0200

    Tests working

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/business/TableBusinessLogic.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/business/TableBusinessLogic.java
@@ -5,10 +5,12 @@ import com.at.ac.tuwien.sepm.ss15.edulium.domain.Table;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.ValidationException;
 import com.at.ac.tuwien.sepm.ss15.edulium.service.ServiceException;
 import javafx.scene.control.Tab;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * Interface for Automatic Table assignment
  */
+@PreAuthorize("isAuthenticated()")
 public interface TableBusinessLogic extends BusinessLogic {
 
     /**
@@ -18,6 +20,7 @@ public interface TableBusinessLogic extends BusinessLogic {
      * is already paid, the table will again be assigned to the user, which took the new order
      * @param table the table, the order has been placed
      */
+    @PreAuthorize("hasRole('SERVICE')")
     public void addedOrderToTable(Table table, Order order) throws ServiceException, ValidationException;
 
     /**
@@ -25,5 +28,6 @@ public interface TableBusinessLogic extends BusinessLogic {
      * table to null.
      * @param table the table, the orders have been paid
      */
+    @PreAuthorize("hasRole('SERVICE')")
     public void paidOrderFromTable(Table table) throws ServiceException, ValidationException;
 }
--- src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/business/TestTableBusinessLogic.java
+++ src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/business/TestTableBusinessLogic.java
@@ -1,7 +1,86 @@
 package com.at.ac.tuwien.sepm.ss15.edulium.business;
 
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.Order;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.Section;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.Table;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.ValidationException;
+import com.at.ac.tuwien.sepm.ss15.edulium.service.InteriorService;
+import com.at.ac.tuwien.sepm.ss15.edulium.service.MenuService;
+import com.at.ac.tuwien.sepm.ss15.edulium.service.OrderService;
+import com.at.ac.tuwien.sepm.ss15.edulium.service.ServiceException;
+import javafx.scene.control.Tab;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by - on 23.06.2015.
  */
 public class TestTableBusinessLogic extends AbstractBusinessLogicTest {
+    @Autowired
+    private InteriorService interiorService;
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private TableBusinessLogic tableBusinessLogic;
+    @Autowired
+    private MenuService menuService;
+
+    private Order createOrder(BigDecimal value, String additionalInformation,
+                              BigDecimal taxRate, LocalDateTime time, Order.State state, int MenuEntryID) throws ServiceException{
+
+        Order order = new Order();
+        order.setTable(interiorService.getAllTables().get(1));
+        order.setMenuEntry(menuService.getAllMenuEntries().get(MenuEntryID));
+        order.setBrutto(value);
+        order.setTax(taxRate);
+        order.setAdditionalInformation(additionalInformation);
+        order.setTime(time);
+        order.setState(state);
+
+        return order;
+    }
+
+
+    @Test
+    @WithMockUser(username = "servicetester", roles={"SERVICE"})
+    public void testAddedOrderToTable_ShouldSetTableUserToOrderUser() throws ValidationException, ServiceException{
+        //Prepare
+        Order order = createOrder(BigDecimal.valueOf(500), "Order Information", BigDecimal.valueOf(0.2),
+                LocalDateTime.now(), Order.State.QUEUED, 1);
+        orderService.addOrder(order);
+        Section section = new Section();
+        section.setName("New Section");
+        interiorService.addSection(section);
+        Table table = new Table();
+        table.setColumn(100);
+        table.setRow(100);
+        table.setSeats(5);
+        table.setSection(section);
+        table.setNumber(50L);
+        interiorService.addTable(table);
+
+        //WHEN
+        order.setTable(table);
+        orderService.updateOrder(order);
+        tableBusinessLogic.addedOrderToTable(table,order);
+
+        //THEN
+        List<Table> results = interiorService.findTables(Table.withIdentity(section, table.getNumber()));
+
+        assertEquals(1, results.size());
+        assertEquals(orderService.getOrderSubmitter(order), results.get(0).getUser());
+    }
+
+    @Test
+    public void testPaidOrderFromTable_ShouldSetTableUserToNull(){
+
+    }
 }
