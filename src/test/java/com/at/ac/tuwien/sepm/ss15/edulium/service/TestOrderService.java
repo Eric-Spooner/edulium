982aef71ef5ed01dc72191d4c47945f5eb252270
Eric.Moerth <e1325306@student.tuwien.ac.at>
2015-06-24 17:04:42 +0200

    Test Order little changes

--- src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/service/TestOrderService.java
+++ src/test/java/com/at/ac/tuwien/sepm/ss15/edulium/service/TestOrderService.java
@@ -3,6 +3,8 @@ package com.at.ac.tuwien.sepm.ss15.edulium.service;
 import com.at.ac.tuwien.sepm.ss15.edulium.business.TableBusinessLogic;
 import com.at.ac.tuwien.sepm.ss15.edulium.dao.DAO;
 import com.at.ac.tuwien.sepm.ss15.edulium.dao.DAOException;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.Menu;
+import com.at.ac.tuwien.sepm.ss15.edulium.domain.MenuEntry;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.Order;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.history.History;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.validation.ValidationException;
@@ -63,7 +65,8 @@ public class TestOrderService extends AbstractServiceTest {
 
         Order order = new Order();
         order.setTable(interiorService.getAllTables().get(1));
-        order.setMenuEntry(menuService.getAllMenuEntries().get(MenuEntryID));
+        System.out.println(menuService.getAllMenuEntries());
+        order.setMenuEntry(menuService.findMenuEntry(MenuEntry.withIdentity(MenuEntryID)).get(0));
         order.setBrutto(value);
         order.setTax(taxRate);
         order.setAdditionalInformation(additionalInformation);
