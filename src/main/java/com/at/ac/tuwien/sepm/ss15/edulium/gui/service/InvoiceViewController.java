6ef7412c7f716b707c530d0cec5990f547cf53f9
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-06-24 18:41:27 +0200

    make it possible to select/deselect items in invoice view

--- src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/gui/service/InvoiceViewController.java
+++ src/main/java/com/at/ac/tuwien/sepm/ss15/edulium/gui/service/InvoiceViewController.java
@@ -4,6 +4,7 @@ import com.at.ac.tuwien.sepm.ss15.edulium.domain.Invoice;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.Order;
 import com.at.ac.tuwien.sepm.ss15.edulium.domain.Table;
 import com.at.ac.tuwien.sepm.ss15.edulium.gui.util.PollingList;
+import com.at.ac.tuwien.sepm.ss15.edulium.service.InvoiceService;
 import com.at.ac.tuwien.sepm.ss15.edulium.service.OrderService;
 import com.at.ac.tuwien.sepm.ss15.edulium.service.ServiceException;
 import javafx.collections.*;
@@ -12,7 +13,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.apache.logging.log4j.LogManager;
@@ -23,6 +26,7 @@ import org.springframework.stereotype.Controller;
 import javax.annotation.Resource;
 import java.net.URL;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller
 public class InvoiceViewController  implements Initializable {
@@ -39,15 +43,11 @@ public class InvoiceViewController  implements Initializable {
     @FXML
     private HBox headerLayout;
 
-    @Resource(name = "taskScheduler")
-    private TaskScheduler taskScheduler;
-
-    private PollingList<Order> allOrders;
-
     private class OrderCell extends ListCell<Order> {
         private final Label nameLabel;
         private final Label additionalInformationLabel;
         private final Label amountLabel;
+        private final Label availableLabel;
         private final Button increaseAmountButton;
         private final Button decreaseAmountButton;
         private final HBox layout;
@@ -64,6 +64,13 @@ public class InvoiceViewController  implements Initializable {
 
             amountLabel = new Label();
 
+            availableLabel = new Label();
+            availableLabel.setStyle("-fx-font-size: 14px;");
+
+            VBox amountLayout = new VBox();
+            amountLayout.setAlignment(Pos.CENTER_RIGHT);
+            amountLayout.getChildren().setAll(amountLabel, availableLabel);
+
             increaseAmountButton = new Button();
             increaseAmountButton.setText("+");
             increaseAmountButton.setMinSize(40, 40);
@@ -72,11 +79,11 @@ public class InvoiceViewController  implements Initializable {
             decreaseAmountButton = new Button();
             decreaseAmountButton.setText("-");
             decreaseAmountButton.setMinSize(40, 40);
-            decreaseAmountButton.setOnAction(action -> ordersForInvoice.computeIfPresent(order, (key, amount) -> amount == 1 ? null : amount - 1));
+            decreaseAmountButton.setOnAction(action -> ordersForInvoice.computeIfPresent(order, (key, amount) -> amount - 1));
 
             layout = new HBox();
             layout.setSpacing(5);
-            layout.getChildren().setAll(increaseAmountButton, decreaseAmountButton, amountLabel, labelLayout);
+            layout.getChildren().setAll(increaseAmountButton, decreaseAmountButton, amountLayout, labelLayout);
 
             setGraphic(layout);
         }
@@ -88,11 +95,22 @@ public class InvoiceViewController  implements Initializable {
             order = item;
 
             if (order != null) {
-                Integer amount = orders.get(order);
+                Integer amount = ordersForInvoice.get(order);
+                Integer maximum = orders.get(order);
+
+                assert amount != null;
+                assert maximum != null;
+
+                Integer available = maximum - amount;
+
+                increaseAmountButton.setDisable(available.equals(0));
+                decreaseAmountButton.setDisable(amount.equals(0));
 
                 nameLabel.setText(order.getMenuEntry().getName());
                 additionalInformationLabel.setText(order.getAdditionalInformation());
-                amountLabel.setText(amount == null ? "-" : amount.toString());
+                amountLabel.setText(amount.toString());
+                availableLabel.setText("(" + available.toString() + ")");
+
                 layout.setVisible(true);
             } else {
                 layout.setVisible(false);
@@ -102,11 +120,17 @@ public class InvoiceViewController  implements Initializable {
 
     @Resource(name = "orderService")
     private OrderService orderService;
+    @Resource(name = "invoiceService")
+    private InvoiceService invoiceService;
+    @Resource(name = "taskScheduler")
+    private TaskScheduler taskScheduler;
+
+    private PollingList<Order> allOrders;
 
     private Table table;
 
-    private final ObservableMap<Order, Integer> orders = FXCollections.observableHashMap();
-    private final Map<Order, Integer> ordersForInvoice = new HashMap<>();
+    private final Map<Order, Integer> orders = new HashMap<>();
+    private final ObservableMap<Order, Integer> ordersForInvoice = FXCollections.observableHashMap();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -119,10 +143,9 @@ public class InvoiceViewController  implements Initializable {
     private void initializeAllOrders() {
         allOrders = new PollingList<>(taskScheduler);
         allOrders.setInterval(1000);
-
         allOrders.addListener((ListChangeListener<Order>) change -> {
             while (change.next()) {
-                for (Order order : clone(change.getRemoved())) {
+                for (Order order : cloneOrders(change.getRemoved())) {
                     Integer count = orders.get(order);
                     if (count == null) {
                         continue;
@@ -134,7 +157,7 @@ public class InvoiceViewController  implements Initializable {
                         ordersForInvoice.compute(order, (key, value) -> (value >= count ? count - 1 : value));
                     }
                 }
-                for (Order order : clone(change.getAddedSubList())) {
+                for (Order order : cloneOrders(change.getAddedSubList())) {
                     Integer count = orders.get(order);
                     if (count == null) {
                         orders.put(order, 1);
@@ -147,7 +170,7 @@ public class InvoiceViewController  implements Initializable {
         });
     }
 
-    private List<Order> clone(List<? extends Order> orderList) {
+    private List<Order> cloneOrders(List<? extends Order> orderList) {
         List<Order> cloned = new ArrayList<>();
         for (Order o : orderList) {
             Order order = new Order();
@@ -165,7 +188,7 @@ public class InvoiceViewController  implements Initializable {
     private void initializeOrdersView() {
         // put all keys of the orders map into a list so that we can use it in the list view
         ObservableList<Order> displayedOrders = FXCollections.observableArrayList();
-        orders.addListener((MapChangeListener<Order, Integer>) change -> {
+        ordersForInvoice.addListener((MapChangeListener<Order, Integer>) change -> {
             displayedOrders.removeAll(change.getKey());
             if (change.wasAdded()) {
                 displayedOrders.add(change.getKey());
@@ -189,20 +212,32 @@ public class InvoiceViewController  implements Initializable {
 
     public void setTable(Table table) {
         this.table = table;
+
         allOrders.setSupplier(() -> {
-            Order matcher = new Order();
-            matcher.setTable(table);
             try {
-                return orderService.findOrder(matcher);
+                Order orderMatcher = new Order();
+                orderMatcher.setTable(table);
+                return orderService.findOrder(orderMatcher).stream().filter(order -> {
+                                        // show only orders which don't belong to an invoice yet
+                                        try {
+                                            Invoice invoiceMatcher = new Invoice();
+                                            invoiceMatcher.setOrders(Collections.singletonList(order));
+                                            return invoiceService.findInvoices(invoiceMatcher).isEmpty();
+                                        } catch (ServiceException e) {
+                                            return true;
+                                        }
+                                    }).collect(Collectors.toList());
             } catch (ServiceException e) {
                 return null;
             }
         });
         allOrders.startPolling();
+
         reset();
     }
 
     private void reset() {
+        ordersForInvoice.clear();
         orders.clear();
     }
 }
