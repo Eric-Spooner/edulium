578594cb84bf05143ccb98e5f637d4b1aebec357
Philippos Gorgoris <philipgorgoris@gmail.com>
2015-06-24 23:09:27 +0200

    Adapted changes of the database in the InvoiceDAO

--- src/main/resources/database/create.sql
+++ src/main/resources/database/create.sql
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS Instalment (
 
 CREATE VIEW IF NOT EXISTS InvoiceExtended AS
     SELECT *, ((SELECT SUM(inst.amount)
-                FROM Instalment AS inst JOIN Invoice AS inv ON inst.invoice_ID = inv.ID) >= brutto) AS completed
+                FROM Instalment AS inst JOIN Invoice AS inv ON inst.invoice_ID = inv.ID) >= brutto) AS closed
     FROM Invoice;
 
 CREATE TABLE IF NOT EXISTS RestaurantSection (
